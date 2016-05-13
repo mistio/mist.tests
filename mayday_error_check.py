cb6528b296e03d943a9f1227918fa115b416a6e8
ptzianos <pavlos.tzianos@gmail.com>
2016-05-13 15:37:24 +0300

    fixed import and added comments in mayday error check

--- mayday_error_check.py
+++ mayday_error_check.py
@@ -1,5 +1,8 @@
 import sys
-# import mist.core.tests.gui.environment
+
+# this is import is not used for it's code but rather to make sure that
+# it's own imports are correct
+import tests.gui.core.environment
 
 if __name__ == '__main__':
     print "This is a dummy script. And it works!!"
