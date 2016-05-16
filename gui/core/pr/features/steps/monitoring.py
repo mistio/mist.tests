0c56c53e9ec8f5cdc4b546549af3b5e9bcbf3542
ptzianos <pavlos.tzianos@gmail.com>
2016-05-16 19:36:44 +0300

    moved monitoring feature from mist.core and added fixes

---
+++ gui/core/pr/features/steps/monitoring.py
@@ -0,0 +1,7 @@
+from tests.gui.steps.navigation import *
+from tests.gui.steps.images import *
+from tests.gui.steps.clouds import *
+from tests.gui.steps.machines import *
+from tests.gui.steps.graphs import *
+from tests.gui.steps.popups import *
+from tests.gui.steps.modals import *
