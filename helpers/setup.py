97ae6bedf36c8f781a46cb3ef44cef1549fa0fcf
pchristos <pollalis.ch@gmail.com>
2016-05-30 21:50:24 +0300

    minor changes

--- helpers/setup.py
+++ helpers/setup.py
@@ -1,25 +1,22 @@
 from tests import config
 
 
-def setup_user_if_not_exists(context, user_email):
-    if config.get('SETUP_ENVIRONMENT'):
-        from mist.core.user.models import User
-        from mist.core.user.models import Owner
-        if context.mist_config.get(user_email):
-            user_email = context.mist_config.get(user_email)
+def setup_user_if_not_exists(user_email):
+    if config.SETUP_ENVIRONMENT:
+        from mist.core.user.models import User, Owner
         try:
-            user = Owner.objects.get(email=user_email)
-        except Owner.DoesNotExist:
+            user = User.objects.get(email=user_email)
+        except User.DoesNotExist:
             user = User()
             user.email = user_email
-        user.set_password(context.mist_config['PASSWORD1'])
-        user.status = 'confirmed'
-        user.save()
+            user.set_password(config.PASSWORD1)
+            user.status = 'confirmed'
+            user.save()
         return user
 
 
 def remove_user(context, user_email):
-    if config.get('SETUP_ENVIRONMENT'):
+    if config.SETUP_ENVIRONMENT:
         from mist.core.user.models import Owner
         if context.mist_config.get(user_email):
             user_email = context.mist_config.get(user_email)
@@ -32,11 +29,11 @@ def remove_user(context, user_email):
 def setup_org_if_not_exists(org_name, owner_email, clean_org=True):
     # If clean_org is set to True then all the teams of the organization
     # will be deleted and all the members except the owner.
-    if config.get('SETUP_ENVIRONMENT'):
+    if config.SETUP_ENVIRONMENT:
         owner = setup_user_if_not_exists(owner_email)
-        from mist.core.user.modes import Organization
+        from mist.core.user.models import Organization
         try:
-            org = Organization.objects(name=org_name).get()
+            org = Organization.objects.get(name=org_name)
         except Organization.DoesNotExist:
             org = Organization()
         if clean_org:
@@ -53,10 +50,11 @@ def setup_org_if_not_exists(org_name, owner_email, clean_org=True):
                 org.members.pop()
         org.add_member_to_team('Owners', owner)
         org.save()
+        return org, owner
 
 
 def setup_team(org_name, team_name, team_members=[], clean_policy=True):
-    if config.get('SETUP_ENVIRONMENT'):
+    if config.SETUP_ENVIRONMENT:
         from mist.core.user.models import Team
         from mist.core.user.models import Organization
         org = Organization.objects(name=org_name).get()
@@ -78,10 +76,11 @@ def setup_team(org_name, team_name, team_members=[], clean_policy=True):
             org.add_member_to_team(team_name, team_member)
 
         org.save()
+        return org
 
 
 def setup_team_members(org_name, team_name, team_members=[]):
-    if config.get('SETUP_ENVIRONMENT'):
+    if config.SETUP_ENVIRONMENT:
         from mist.core.user.models import Team
         from mist.core.user.models import Organization
         org = Organization.objects(name=org_name).get()
