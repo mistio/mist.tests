ef87e4b269442bf8fc4f8c4cc99b89c51f24c830
pchristos <pollalis.ch@gmail.com>
2016-05-30 21:51:10 +0300

    substitute code with the new helpers/setup.py

--- api/core/security_tags.py
+++ api/core/security_tags.py
@@ -1,26 +1,22 @@
 from tests.api.helpers import *
 from tests.api.utils import *
 
+from tests.helpers.setup import setup_user_if_not_exists, setup_org_if_not_exists
+from tests.helpers.setup import setup_team
+
 from tests import config
+
 
 # This test requires that the organization `MyOrg` has been created by the user
 
 
-def test_show_user_org_for_owner(pretty_print, mist_core, cache,
-                                 org_name, owner_api_token):
+def test_show_user_org_for_owner(pretty_print, mist_core, cache, org_name,
+                                 owner_email, owner_api_token):
     print "\n>>> GETing /orgs to get a list of all user orgs and the api" \
           "token with access to that org\n"
-    response = mist_core.list_orgs(api_token=owner_api_token).get()
-    assert_response_ok(response)
 
-    org_json = response.json()
-    org_id = None
-    for org in org_json:
-        if org_name == org['name']:
-            org_id = org['id']
-            break
-    assert_is_not_none(org_id)
-    cache.set('rbac/org_id', org_id)
+    org, owner = setup_org_if_not_exists(org_name, owner_email, clean_org=False)
+    cache.set('rbac/org_id', org.id)
 
     response = mist_core.show_user_org(api_token=owner_api_token).get()
     assert_response_ok(response)
@@ -31,12 +27,12 @@ def test_show_user_org_for_owner(pretty_print, mist_core, cache,
     print "Success!!!!"
 
 
-def test_create_personal_tokens_for_member(pretty_print, mist_core,
-                                           cache, member1_email, member1_password):
+def test_create_token_for_member(pretty_print, mist_core, cache,
+                                 member1_email, password1):
     print "\n>>> GETing / to get personal token of team member\n"
 
     response = mist_core.create_token(email=member1_email,
-                                      password=member1_password).post()
+                                      password=password1).post()
     assert_response_ok(response)
     api_token_id = response.json().get('id', None)
     api_token = response.json().get('token', None)
@@ -50,50 +46,20 @@ def test_create_personal_tokens_for_member(pretty_print, mist_core,
 def test_invite_member_to_team(pretty_print, mist_core, cache,
                                owner_api_token, org_name, member1_email):
     print "\n>>> POSTing / to invite member1 to a team\n"
+    user = setup_user_if_not_exists(member1_email)
     org_id = cache.get('rbac/org_id', '')
-    member1_api_token = cache.get('rbac/member1_api_token', '')
 
     # get a random new team name
     response = mist_core.list_teams(org_id=org_id,
                                     api_token=owner_api_token).get()
     assert_response_ok(response)
-    cache.set('rbac/member1_team_name',
-              get_random_team_name(json.loads(response.content)))
+    team_name = get_random_team_name(json.loads(response.content))
+    cache.set('rbac/member1_team_name', team_name)
 
-    # create an new team for member1
-    response = mist_core.add_team(name=cache.get('rbac/member1_team_name', ''),
-                                  description='', org_id=org_id,
-                                  api_token=owner_api_token).post()
-    assert_response_ok(response)
-    rjson = json.loads(response.content)
-    assert_is_not_none(rjson.get('id'),
-                       "Did not get an id back in the response")
-    cache.set('rbac/member1_team_id', rjson['id'])
-
-    # add member1 to new team
-    response = mist_core.invite_member_to_team(org_id=org_id,
-                                               team_id=cache.get('rbac/member1_team_id', ''),
-                                               email=member1_email,
-                                               api_token=owner_api_token).post()
-    assert_response_ok(response)
-    rjson = json.loads(response.content)
-    assert_is_not_none(rjson.get('id'),
-                       "Did not get an id back in the response")
-    cache.set('rbac/member1_id', rjson.get('id'))
-
-    pending = rjson['pending']
-    if pending is True:
-        # this is for member that is already a user in our db
-        # but not member of the org
-        response = mist_core.show_user_invitations(api_token=member1_api_token).get()
-        assert_response_ok(response)
-        rjson = json.loads(response.content)
-        token = rjson[0]['token']
-
-        response = mist_core.confirm_invitation(api_token=member1_api_token,
-                                                invitoken=token).get()
-
-        assert_response_ok(response)
+    org = setup_team(org_name, team_name, [user])
+    for team in org.teams:
+        if team.name == team_name:
+            cache.set('rbac/member1_team_id', team.id)
 
     print "Success!!!!"
 
@@ -206,7 +172,9 @@ def test_modify_security_tags(pretty_print, mist_core, cache,
     # The {'security': 'test'} tag added by the owner in the previous method
     # exists also as part of team policies, thus it should be present in
     # every set_machine_tags request by the team member in order for the
-    # action to be allowed
+    # action to be allowed. Also, the {'sec': 'tes'} tag is a security tag,
+    # but it is not present on any resource, therefore no team member is
+    # allowed to add it.
 
     tags = [
         {'sec': 'test'},
