4695d975bd032b67f939b73aaf78b0855dcb6474
ptzianos <pavlos.tzianos@gmail.com>
2016-05-31 22:03:58 +0300

    multiple fixes for setting up cloud, organization and teams

--- helpers/setup.py
+++ helpers/setup.py
@@ -1,7 +1,7 @@
 from tests import config
 
 
-def setup_user_if_not_exists(user_email):
+def setup_user_if_not_exists(user_email, password=None):
     if config.SETUP_ENVIRONMENT:
         from mist.core.user.models import User, Owner
         try:
@@ -9,9 +9,11 @@ def setup_user_if_not_exists(user_email):
         except User.DoesNotExist:
             user = User()
             user.email = user_email
-            user.set_password(config.PASSWORD1)
-            user.status = 'confirmed'
-            user.save()
+        if not password:
+            password = config.PASSWORD1
+        user.set_password(password)
+        user.status = 'confirmed'
+        user.save()
         return user
 
 
@@ -30,31 +32,51 @@ def setup_org_if_not_exists(org_name, owner_email, clean_org=True):
     # If clean_org is set to True then all the teams of the organization
     # will be deleted and all the members except the owner.
     if config.SETUP_ENVIRONMENT:
-        owner = setup_user_if_not_exists(owner_email)
+        from mist.core.cloud.models import Cloud
+
+        from mist.core.user.models import User
         from mist.core.user.models import Organization
+        owner = User.objects.get(email=owner_email)
         try:
             org = Organization.objects.get(name=org_name)
         except Organization.DoesNotExist:
             org = Organization()
+            org.name = org_name
         if clean_org:
             i = 0
             while len(org.teams) > 1:
                 if org.teams[i].name != 'Owners':
-                    team = org.teams.pop(i)
-                    team.delete()
+                    org.teams.pop(i)
                 else:
-                    while len(team.members) > 0:
-                        team.members.pop()
+                    while len(org.teams[i].members) > 0:
+                        org.teams[i].members.pop()
                     i += 1
             while len(org.members) > 0:
                 org.members.pop()
         org.add_member_to_team('Owners', owner)
         org.save()
+
+        try:
+            Cloud.objects.get(owner=org, title=config.API_TESTING_CLOUD)
+        except Cloud.DoesNotExist:
+            cloud = Cloud()
+            cloud.title = config.API_TESTING_CLOUD
+            cloud.enabled = True
+            cloud.owner = org
+
+            if config.API_TESTING_CLOUD_PROVIDER == 'EC2':
+                cloud.apikey = config.CREDENTIALS['EC2']['api_key']
+                cloud.apisecret = config.CREDENTIALS['EC2']['api_secret']
+                cloud.provider = 'ec2_ap_northeast'
+
+            cloud.save()
+
         return org, owner
 
 
 def setup_team(org_name, team_name, team_members=[], clean_policy=True):
     if config.SETUP_ENVIRONMENT:
+        from mist.core.user.models import User
         from mist.core.user.models import Team
         from mist.core.user.models import Organization
         org = Organization.objects(name=org_name).get()
@@ -73,10 +95,10 @@ def setup_team(org_name, team_name, team_members=[], clean_policy=True):
             org.teams.append(team)
 
         for team_member in team_members:
-            org.add_member_to_team(team_name, team_member)
+            org.add_member_to_team(team_name, User.objects.get(email=team_member))
 
         org.save()
-        return org
+        return org.get_team(team_name).id
 
 
 def setup_team_members(org_name, team_name, team_members=[]):
@@ -87,4 +109,3 @@ def setup_team_members(org_name, team_name, team_members=[]):
         for team_member in team_members:
             org.add_member_to_team(team_name, team_member)
         org.save()
-
