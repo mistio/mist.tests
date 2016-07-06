0a87cbf01e58896f747ea3764a47f998f0b7bad5
Mike Muzurakis <clopy@mist.io>
2016-07-06 10:55:18 +0300

    get number of allowed failures from env

--- check_last_build.py
+++ check_last_build.py
@@ -6,6 +6,7 @@ from email.mime.text import MIMEText
 from config import get_value_of
 
 PRIVATE_TOKEN = os.environ.get('PRIVATE_TOKEN')
+TRIGGER_MAYDAY_ON_FAILURES = os.environ.get('TRIGGER_MAYDAY_ON_FAILURES') or 2
 gl_url = "https://gitlab.ops.mist.io/api/v3/projects/2/builds"
 headers = {"PRIVATE-TOKEN": PRIVATE_TOKEN}
 
@@ -19,27 +20,25 @@ TEXT = ''
 
 request = requests.get(gl_url, headers=headers)
 data = request.json()
-print data[0]
-print "\n"
-print data[1]
-print "\n"
-print data[2]
-print "\n"
-print data[3]
 
+failures = 0
+#Checking twice the logs as we have two stages
+for i in range(TRIGGER_MAYDAY_ON_FAILURES * 2):
+    if data[i]['name'] == 'run_mayday_test' and data[i]['status'] == 'failed':
+        failures +=1
 
-if data[0]['status'] == 'failed' and data[1]['status'] == 'failed':
+if failures >= TRIGGER_MAYDAY_ON_FAILURES:
 
-    print 'Both last tests failed, raising mayday'
+    print str(TRIGGER_MAYDAY_ON_FAILURES) + ' last tests failed, raising mayday'
 
-    # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, TO, SUBJECT, TEXT)
-
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
     server.login(FROM,gmail_pwd)
-
 #    server.sendmail(FROM, TO, message)
     server.quit()
+
+else:
+    print 'One of the ' + str(TRIGGER_MAYDAY_ON_FAILURES) + ' last tests succeeded, not raising mayday'
