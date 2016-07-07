b15813da5f6908b87631e06462b821e2c2f6e86c
ptzianos <pavlos.tzianos@gmail.com>
2016-07-07 16:40:26 +0300

    made test logs dir configurable in run_and_commit script

--- gitlab/run_test_or_commit.sh
+++ gitlab/run_test_or_commit.sh
@@ -16,16 +16,16 @@ else
   git config --global user.name "mistio-gitlab"
   git config --global user.email gitlab.ops.mist.io@gmail.com
   git clone git@104.196.122.66:mistio/mist.test.logs.git /logs
-  mkdir "/logs/mist.core/$CI_BUILD_ID"
-  cp /var/log/*-std* "/logs/mist.core/$CI_BUILD_ID"
-  cp /var/log/js_console.log "/logs/mist.core/$CI_BUILD_ID"
-  cp /var/log/rabbitmq/* "/logs/mist.core/$CI_BUILD_ID"
-  cp /var/log/supervisord.log "/logs/mist.core/$CI_BUILD_ID"
-  cp /var/log/error* "/logs/mist.core/$CI_BUILD_ID"
+  mkdir -p "/logs/$MIST_TEST_LOG_DIR"
+  cp /var/log/*-std* "/logs/$MIST_TEST_LOG_DIR"
+  cp /var/log/js_console.log "/logs/$MIST_TEST_LOG_DIR"
+  cp /var/log/rabbitmq/* "/logs/$MIST_TEST_LOG_DIR"
+  cp /var/log/supervisord.log "/logs/$MIST_TEST_LOG_DIR"
+  cp /var/log/error* "/logs/$MIST_TEST_LOG_DIR"
   cd /logs
-  git add "mist.core/$CI_BUILD_ID"
+  git add "$MIST_TEST_LOG_DIR"
   git commit -m "Logs for build $CI_BUILD_ID"
   git push origin master
-  echo "Pushed logs and other stuff to https://gitlab.ops.mist.io/mistio/mist.test.logs/tree/master/mist.core/$CI_BUILD_ID"
+  echo "Pushed logs and other stuff to https://gitlab.ops.mist.io/mistio/mist.test.logs/tree/master/$MIST_TEST_LOG_DIR"
 fi
 exit $exit_code
