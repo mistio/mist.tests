4bd8ba1f57b2f898d858908fc1dfda4b658591d7
ptzianos <pavlos.tzianos@gmail.com>
2016-07-19 12:15:18 +0300

    removed duplicate steps from polymist_clouds module

--- gui/steps/polymist_clouds.py
+++ gui/steps/polymist_clouds.py
@@ -14,21 +14,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-@step(u'I click the Add provider button')
-def click_polymer_button(context):
-    import ipdb
-    ipdb.set_trace()
-    container = context.browser.find_element_by_xpath("//div[contains(@class, 'cloud-add')]")
-    button = container.find_element_by_tag_name('paper-button')
-    ActionChains(context.browser).move_to_element(button).click().perform()
-
-
-@step(u'I click the provider button {provider}')
-def open_provider_drop_down(context, provider):
-    button = context.browser.find_element_by_xpath('//span[contains(text(), "%s")]' %str(provider))
-    ActionChains(context.browser).move_to_element(button).click().perform()
-
-
 @step(u'I expect for "{element_text}" label to be visible within max {seconds} '
       u'seconds')
 def element_label_become_visible_waiting_with_timeout(context, element_text, seconds):
