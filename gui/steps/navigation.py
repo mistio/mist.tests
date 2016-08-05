10adb636e0632392a837306420ad8888c286c76c
ptzianos <pavlos.tzianos@gmail.com>
2016-08-05 21:16:14 +0300

    moved organizational context from navigation module

--- gui/steps/navigation.py
+++ gui/steps/navigation.py
@@ -415,31 +415,3 @@ def wait_for_some_list_page_to_load(context, title):
 def get_gravatar(context):
     return context.browser.find_element_by_css_selector(
         'paper-icon-button.gravatar')
-
-
-def get_current_context(context):
-    return safe_get_element_text(context.browser.find_element_by_css_selector(
-            'div.current.context').find_element_by_tag_name('h4')).strip().lower()
-
-
-@step(u'I ensure that I am in the "{organization}" organization context')
-def ensure_organizational_context(context, organization):
-    context.execute_steps(u'''
-        Then I click the Gravatar
-        And I wait for 1 seconds
-    ''')
-    organization = organization.strip().lower()
-    if get_current_context(context) == organization:
-        return True
-    else:
-        buttons = context.browser.find_element_by_id('topBar'). \
-            find_element_by_id('dropdown').\
-            find_elements_by_tag_name('paper-item')
-        click_button_from_collection(context, organization, buttons)
-        context.execute_steps(u'''
-            Then I wait for the dashboard to load
-            And I click the Gravatar
-            And I wait for 1 seconds
-        ''')
-    assert get_current_context(context) == organization, \
-        "Organizational context has not been changed"
