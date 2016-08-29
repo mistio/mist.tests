f52a359919142095fb8f18f8137bbb482c51338c
ptzianos <pavlos.tzianos@gmail.com>
2016-08-29 21:17:54 +0300

    PEP8 fix

--- gui/steps/scroll.py
+++ gui/steps/scroll.py
@@ -1,5 +1,6 @@
 from behave import step
 
+
 @step(u'I scroll the clouds list into view')
 def scroll_to_top(context):
     context.browser.execute_script("a = document.querySelector('#cloudslist'); if (a != null) { a.scrollIntoView() };")
