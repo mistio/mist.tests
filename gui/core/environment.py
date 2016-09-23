2fac99fca8b8c95d57d64c83b6caf0c0134830ce
ptzianos <pavlos.tzianos@gmail.com>
2016-09-23 20:36:46 +0300

    added code for starting ffmpeg and terminating process along with the behave tests

--- gui/core/environment.py
+++ gui/core/environment.py
@@ -1,5 +1,12 @@
+import os
 import sys
 import logging
+import subprocess
+
+from shlex import split
+
+from time import time
+from time import sleep
 
 from tests import config
 
@@ -66,6 +73,18 @@ def before_all(context):
         context.mail_path = config.MAIL_PATH
         # calling behaving to setup it's context variables.
         behaving_mail.before_all(context)
+    if config.RECORD_SELENIUM:
+        if os.path.isfile('test.mp4'):
+            os.remove('test.mp4')
+        command = 'ffmpeg -f x11grab -video_size 1024x768 ' \
+                  '-i 127.0.0.1:{0} -codec:v libx264 -r 12 ' \
+                  'test.mp4'.format(config.DISPLAY_NUM)
+        context.mist_config['recording_proc'] = subprocess.Popen(split(command),
+                                                                 stdout=subprocess.PIPE,
+                                                                 stderr=subprocess.STDOUT,
+                                                                 stdin=subprocess.PIPE,
+                                                                 bufsize=0)
+
     log.info("Finished with before_all hook. Starting tests")
 
 
@@ -89,3 +108,23 @@ def after_all(context):
     context.mist_config['browser'].quit()
     if context.mist_config.get('browser2'):
         context.mist_config['browser2'].quit()
+    if context.mist_config.get('recording_proc'):
+        proc = context.mist_config['recording_proc']
+        return_code = proc.poll()
+        if not return_code:
+            proc.communicate('q\n')
+            log.info("Sent terminating character to recording process")
+            timeout = time() + 20
+            while time() < timeout:
+                if proc.poll() is not None:
+                    log.info("Recording process has terminated")
+                    break
+                log.info("Waiting for recording process to terminate")
+                sleep(1)
+            else:
+                proc.kill()
+                raise Exception("Could not correctly terminate subprocess for "
+                                "selenium recording")
+        else:
+            raise Exception("Seems like subprocess for selenium recording "
+                            "terminated before the end of the tests")
