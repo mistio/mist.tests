c8bc2c6d8f65b125e37eedd3470e1ada9f715c13
ptzianos <pavlos.tzianos@gmail.com>
2016-09-26 18:02:40 +0300

    changed environemnt to use new module for recording session

--- gui/core/environment.py
+++ gui/core/environment.py
@@ -1,18 +1,13 @@
-import os
 import sys
 import logging
-import subprocess
-
-from shlex import split
-
-from time import time
-from time import sleep
 
 from tests import config
 
 from tests.helpers.selenium_utils import choose_driver
-from tests.helpers.selenium_utils import get_screenshot
 from tests.helpers.selenium_utils import dump_js_console_log
+
+from tests.helpers.recording import start_recording
+from tests.helpers.recording import stop_recording
 
 log = logging.getLogger(__name__)
 
@@ -73,17 +68,9 @@ def before_all(context):
         context.mail_path = config.MAIL_PATH
         # calling behaving to setup it's context variables.
         behaving_mail.before_all(context)
+
     if config.RECORD_SELENIUM:
-        if os.path.isfile('test.mp4'):
-            os.remove('test.mp4')
-        command = 'ffmpeg -f x11grab -video_size 1024x768 ' \
-                  '-i 127.0.0.1:{0} -codec:v libx264 -r 12 ' \
-                  'test.mp4'.format(config.DISPLAY_NUM)
-        context.mist_config['recording_proc'] = subprocess.Popen(split(command),
-                                                                 stdout=subprocess.PIPE,
-                                                                 stderr=subprocess.STDOUT,
-                                                                 stdin=subprocess.PIPE,
-                                                                 bufsize=0)
+        context.mist_config['recording_proc'] = start_recording()
 
     log.info("Finished with before_all hook. Starting tests")
 
@@ -98,33 +85,10 @@ def before_feature(context, feature):
             raise e
 
 
-def after_scenario(context, scenario):
-    if scenario.status == 'failed':
-        get_screenshot(context)
-
-
 def after_all(context):
     dump_js_console_log(context)
     context.mist_config['browser'].quit()
     if context.mist_config.get('browser2'):
         context.mist_config['browser2'].quit()
     if context.mist_config.get('recording_proc'):
-        proc = context.mist_config['recording_proc']
-        return_code = proc.poll()
-        if not return_code:
-            proc.communicate('q\n')
-            log.info("Sent terminating character to recording process")
-            timeout = time() + 20
-            while time() < timeout:
-                if proc.poll() is not None:
-                    log.info("Recording process has terminated")
-                    break
-                log.info("Waiting for recording process to terminate")
-                sleep(1)
-            else:
-                proc.kill()
-                raise Exception("Could not correctly terminate subprocess for "
-                                "selenium recording")
-        else:
-            raise Exception("Seems like subprocess for selenium recording "
-                            "terminated before the end of the tests")
+        stop_recording(context.mist_config['recording_proc'])
