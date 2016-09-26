48d481a2d5e25aa2b5f0b3bb05fe1b10a35bac05
ptzianos <pavlos.tzianos@gmail.com>
2016-09-26 19:10:48 +0300

    fixed string formatting

--- helpers/recording.py
+++ helpers/recording.py
@@ -17,9 +17,9 @@ def start_recording(output='test.mp4', dimension='1024x768',
                     display_num='1'):
     if os.path.isfile(output):
         os.remove(output)
-    command = 'ffmpeg -f x11grab -video_size %s ' \
-              '-i 127.0.0.1:{0} -codec:v libx264 -r 12 ' \
-              '%s'.format(display_num, dimension, output)
+    command = 'ffmpeg -f x11grab -video_size {0} ' \
+              '-i 127.0.0.1:{1} -codec:v libx264 -r 12 ' \
+              '{2}'.format(dimension, display_num, output)
     return subprocess.Popen(split(command), stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT,
                             stdin=subprocess.PIPE, bufsize=0)
