c0ddfc6a962f55119685f66cd16bfc5321f69620
spiros <spirostza77@hotmail.com>
2016-10-23 20:35:31 +0300

    debugging

--- helpers/recording.py
+++ helpers/recording.py
@@ -69,6 +69,7 @@ def stop_recording():
     global kill_recording_process
     global recording_sub_process
     kill_recording_process = True
+    log.info("Stopping recording of the session")
     recording_sub_process.stdin.write('q\n')
     log.info("Sent terminating character to recording process")
     recording_process_lock.acquire()
