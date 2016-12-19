12e1e520675ab3880e2841d62c87aa994520bc66
spiros <spirostza77@hotmail.com>
2016-12-19 23:18:36 +0200

    eliminate unnecessary step+import ssh.py

--- misttests/gui/core/pr/features/steps/machine_create.py
+++ misttests/gui/core/pr/features/steps/machine_create.py
@@ -3,3 +3,4 @@ from misttests.gui.steps.machines import *
 from misttests.gui.steps.modals import *
 from misttests.gui.steps.tags import *
 from misttests.gui.steps.popups import *
+from misttests.gui.steps.ssh import *
