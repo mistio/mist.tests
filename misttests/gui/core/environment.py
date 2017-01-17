97a3c56bb23d28c7d7cc0aa81355f30e63694a34
Mike Muzurakis <clopy@mist.io>
2017-01-17 21:44:20 +0200

    comment out stop recording too

--- misttests/gui/core/environment.py
+++ misttests/gui/core/environment.py
@@ -115,5 +115,5 @@ def finish_and_cleanup(context):
     context.mist_config['browser'].quit()
     if context.mist_config.get('browser2'):
         context.mist_config['browser2'].quit()
-    if context.mist_config.get('recording_session', False):
-        stop_recording()
+    #if context.mist_config.get('recording_session', False):
+    #    stop_recording()
