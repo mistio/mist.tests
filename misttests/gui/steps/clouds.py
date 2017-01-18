cd62f7c69039e0de539f60db4adab226ebad94df
spiros <spirostza77@hotmail.com>
2017-01-19 00:09:10 +0200

    find tags paper-item in class named selectable content

--- misttests/gui/steps/clouds.py
+++ misttests/gui/steps/clouds.py
@@ -280,6 +280,20 @@ cloud_second_creds_dict = {
 }
 
 
+@step(u'I select the "{provider}" provider')
+def select_provider_in_cloud_add_form(context, provider):
+    provider_title = provider.lower()
+    import ipdb;ipdb.set_trace()
+    clouds_class = context.browser.find_element_by_class_name('selectable-content')
+    clouds = clouds_class.find_elements_by_tag_name('paper-item')
+    for c in clouds:
+        try:
+            if safe_get_element_text(c).lower().strip() == provider_title:
+                return c
+        except (NoSuchElementException, StaleElementReferenceException):
+            pass
+
+
 @step(u'I use my "{provider}" credentials')
 def cloud_creds(context, provider):
     provider = provider.strip().lower()
