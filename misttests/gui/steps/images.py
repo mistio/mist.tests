05be214773a0514dc627fa4bc8b4fde450131d37
spiros <spirostza77@hotmail.com>
2017-01-21 22:44:45 +0200

    delete unused methods

--- misttests/gui/steps/images.py
+++ misttests/gui/steps/images.py
@@ -44,18 +44,6 @@ def assert_starred_unstarred_image(context,image,state,seconds):
                   % (image, state, seconds)
 
 
-@step(u'an image that contains "{text}" should be starred')
-def assert_starred_image(context, text):
-    images_list = context.browser.find_element_by_id("image-list")
-    images = images_list.find_elements_by_tag_name("li")
-    starred_images = filter(lambda li: 'staron' in li.get_attribute('class'), images)
-    if context.mist_config.get(text):
-        text = context.mist_config[text]
-    starred_image = filter(lambda li: text in safe_get_element_text(li).lower(), starred_images)
-    assert len(starred_image) == 1, \
-        "Could not find starred image with name %s" % text
-
-
 @step(u'I star the "{text}" image')
 def star_image(context, text):
     images_list = context.browser.find_element_by_id("items")
@@ -67,13 +55,6 @@ def star_image(context, text):
             image = image.find_element_by_tag_name('h3')
             context.mist_config['the_image_name_i_starred'] = safe_get_element_text(image)
             return
-
-
-@step(u'I clear the images search bar')
-def clear_image_search_bar(context):
-    search_bar = context.browser.find_element_by_id("search-term-input")
-    for i in range(20):
-        search_bar.send_keys(u'\ue003')
 
 
 def scroll_down_and_wait(context, wait_for_unstarred_images=False, wait=5):
@@ -95,15 +76,3 @@ def scroll_down_and_wait(context, wait_for_unstarred_images=False, wait=5):
             return True
 
     return False
-
-
-@step(u'I scroll down until all starred images appear')
-def get_all_starred_images(context):
-    while scroll_down_and_wait(context):
-        pass
-
-
-@step(u'I scroll down until all images appear')
-def get_all_images(context):
-    while scroll_down_and_wait(context, wait_for_unstarred_images=True):
-        pass
