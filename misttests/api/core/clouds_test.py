0cb63caa2bec73fa17e2b408f8e2d3cde5b41bb8
spiros <spirostza77@hotmail.com>
2017-02-07 18:55:04 +0200

    add_cloud done

--- misttests/api/core/clouds_test.py
+++ misttests/api/core/clouds_test.py
@@ -1,4 +1,6 @@
 from misttests.api.helpers import *
+from misttests import config
+
 
 
 ############################################################################
@@ -23,4 +25,14 @@ def test_add_cloud_wrong_api_token(pretty_print, mist_core, owner_api_token):
     print "Success!!!"
 
 
-    
+def test_add_cloud_no_api_token(pretty_print, mist_core):
+    response = mist_core.add_cloud("Openstack", 'openstack').post()
+    assert_response_forbidden(response)
+    print "Success!!!"
+
+
+def test_add_cloud_ok(pretty_print, mist_core, owner_api_token):
+    response = mist_core.add_cloud('Linode', 'linode', api_token=owner_api_token,
+                                   api_key=config.CREDENTIALS['LINODE']['api_key']).post()
+    assert_response_ok(response)
+    print "Success!!!"
