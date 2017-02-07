7d660dbc1a51cbc5fb0451b74651a3b8a71c720f
spiros <spirostza77@hotmail.com>
2017-02-07 19:35:15 +0200

    rename_cloud completed

--- misttests/api/core/clouds_test.py
+++ misttests/api/core/clouds_test.py
@@ -65,3 +65,22 @@ def test_rename_cloud_no_api_token(pretty_print, mist_core, owner_api_token):
                                       api_token='00' + owner_api_token[:-2]).put()
     assert_response_unauthorized(response)
     print "Success!!!"
+
+
+def test_rename_cloud_missing_parameter(pretty_print, mist_core, owner_api_token):
+    response = mist_core.rename_cloud(cloud_id='dummy', new_name='',
+                                      api_token=owner_api_token).put()
+    assert_response_not_found(response)
+    print "Success!!!"
+
+
+def test_delete_cloud_wrong_id(pretty_print, mist_core, owner_api_token):
+    response = mist_core.delete_cloud(cloud_id='dummy',api_token=owner_api_token).delete()
+    assert_response_not_found(response)
+    print "Success"
+
+
+def test_delete_cloud_no_api_token(pretty_print, mist_core):
+    response = mist_core.delete_cloud(cloud_id='dummy').delete()
+    assert_response_forbidden(response)
+    print "Success!!!"
