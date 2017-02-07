ee10ea77916cc977d6ce139f5278e7e97abfcb3b
spiros <spirostza77@hotmail.com>
2017-02-07 18:28:24 +0200

    test_add cloud added

--- misttests/api/core/clouds_test.py
+++ misttests/api/core/clouds_test.py
@@ -11,6 +11,16 @@ def test_list_clouds(pretty_print, mist_core, owner_api_token):
     print "Success!!!"
 
 
-def test_list_images(pretty_print, mist_core, owner_api_token):
-    response = mist_core.list_images('dummy', api_token=owner_api_token).get()
-    assert_response_ok(response)
+def test_add_cloud_missing_parameter(pretty_print, mist_core, owner_api_token):
+    response = mist_core.add_cloud("Openstack", 'openstack', api_token=owner_api_token).post()
+    assert_response_bad_request(response)
+    print "Success!!!"
+
+
+def test_add_cloud_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.add_cloud("Openstack", 'openstack', api_token='00' + owner_api_token[:-2]).post()
+    assert_response_unauthorized(response)
+    print "Success!!!"
+
+
+    
