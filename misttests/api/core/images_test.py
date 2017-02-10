b66effad22f59818e6fffc63a3042baa5ceca951
spiros <spirostza77@hotmail.com>
2017-02-11 01:15:13 +0200

    star image left

--- misttests/api/core/images_test.py
+++ misttests/api/core/images_test.py
@@ -23,3 +23,22 @@ def test_list_images(pretty_print, mist_core, owner_api_token, cache):
     response = mist_core.list_images(cloud_id=cache.get('cloud_id',''), api_token=owner_api_token).post()
     assert_response_ok(response)
     print "Success!!!"
+
+
+def test_list_images_wrong_api_token(pretty_print, mist_core, cache, owner_api_token):
+    response = mist_core.list_images(cloud_id=cache.get('cloud_id', ''), api_token=owner_api_token + 'dummy').post()
+    assert_response_unauthorized(response)
+    print "Success!!!"
+
+
+def test_list_images_no_api_token(pretty_print, cache, mist_core):
+    response = mist_core.list_images(cloud_id=cache.get('cloud_id', '')).post()
+    assert_response_forbidden(response)
+    print "Success!!!"
+
+
+# def test_list_images_wrong_cloud_id(pretty_print, mist_core, owner_api_token):
+#     response = mist_core.list_images(cloud_id='dummy', api_token=owner_api_token).post()
+#     assert_response_not_found(response)
+#     print "Success!!!"
+
