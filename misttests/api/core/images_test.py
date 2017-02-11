ce1b6475819e45170c245b9b2a7abcbc13a534f5
spiros <spirostza77@hotmail.com>
2017-02-11 14:20:07 +0200

    list_images_wrong id commented out

--- misttests/api/core/images_test.py
+++ misttests/api/core/images_test.py
@@ -7,7 +7,8 @@ from misttests import config
 ############################################################################
 
 
-def test_add_cloud_ok(pretty_print, cache, mist_core, owner_api_token):
+# needs to change in the backend: get instead of post...
+def test_list_images(pretty_print, mist_core, owner_api_token, cache):
     response = mist_core.add_cloud('Linode', 'linode', api_token=owner_api_token,
                                    api_key=config.CREDENTIALS['LINODE']['api_key']).post()
     assert_response_ok(response)
@@ -15,13 +16,9 @@ def test_add_cloud_ok(pretty_print, cache, mist_core, owner_api_token):
     assert_response_ok(response)
     assert len(response.json()) == 1
     cache.set('cloud_id', response.json()[0]['id'])
-    print "Success!!!"
-
-
-# needs to change in the backend
-def test_list_images(pretty_print, mist_core, owner_api_token, cache):
     response = mist_core.list_images(cloud_id=cache.get('cloud_id',''), api_token=owner_api_token).post()
     assert_response_ok(response)
+    assert len(response.json()) > 0, "No images are listed for Linode cloud"
     print "Success!!!"
 
 
@@ -36,7 +33,7 @@ def test_list_images_no_api_token(pretty_print, cache, mist_core):
     assert_response_forbidden(response)
     print "Success!!!"
 
-
+#needs to change in the backend: return 404instead of 500....
 # def test_list_images_wrong_cloud_id(pretty_print, mist_core, owner_api_token):
 #     response = mist_core.list_images(cloud_id='dummy', api_token=owner_api_token).post()
 #     assert_response_not_found(response)
