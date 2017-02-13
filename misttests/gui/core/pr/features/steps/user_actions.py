b6b7f99f0a63fc7c28a0b29cc53d9da57a75b313
spiros <spirostza77@hotmail.com>
2017-02-14 01:18:46 +0200

    insert proper steps at pr feaures

--- misttests/gui/core/pr/features/steps/user_actions.py
+++ misttests/gui/core/pr/features/steps/user_actions.py
@@ -9,3 +9,4 @@ from misttests.gui.steps.browser import *
 from misttests.gui.steps.sessions import *
 from misttests.gui.steps.api_tokens import *
 from misttests.gui.steps.setup import *
+from misttests.gui.steps.sso import *
