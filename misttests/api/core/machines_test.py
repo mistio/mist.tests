8bb5a541e1e02f0fa1ebd2761fcfd1f3842bd5cb
spiros <spirostza77@hotmail.com>
2017-02-16 18:14:25 +0200

    create_machine unit tests done

--- misttests/api/core/machines_test.py
+++ misttests/api/core/machines_test.py
@@ -8,12 +8,39 @@ import pytest
 #                             Unit Testing                                 #
 ############################################################################
 
-
+# should get forbidden?
 def test_list_machines_no_api_token(pretty_print, mist_core):
     response = mist_core.list_machines(cloud_id='dummy').get()
-    assert_response_forbidden(response)
+    assert_response_unauthorized(response)
     print "Success!!!"
 
+
+def test_list_machines_wrong_api_token(pretty_print, mist_core):
+    response = mist_core.list_machines(cloud_id='dummy', api_token='dummy').get()
+    assert_response_unauthorized(response)
+    print "Success!!!"
+
+# internal server error? wtf????
+# def test_list_machines_wrong_cloud_id(pretty_print, mist_core, owner_api_token):
+#     response = mist_core.list_machines(cloud_id='dummy', api_token=owner_api_token).get()
+#     assert_response_not_found(response)
+#     print "Success!!!"
+
+
+# ask below, if image == '', then not found...
+def test_create_machine_wrong_api_token(pretty_print, mist_core):
+    response = mist_core.create_machine(cloud_id='dummy', api_token='dummy',
+                                        key_id='', name='', provider='', location='',
+                                        image='dummy', size='',).post()
+    assert_response_unauthorized(response)
+    print "Success!!!"
+
+def test_create_machine_no_api_token(pretty_print, mist_core):
+    response = mist_core.create_machine(cloud_id='dummy', key_id='',
+                                        name='', provider='', location='',
+                                        image='dummy', size='',).post()
+    assert_response_forbidden(response)
+    print "Success!!!"
 
 
 # def test_add_cloud_missing_parameter(pretty_print, mist_core, owner_api_token):
