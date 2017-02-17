2b626f7c8f60cc9ce2437177a176dd031e6f3507
spiros <spirostza77@hotmail.com>
2017-02-17 15:01:03 +0200

    reemove ipdb

--- misttests/api/core/machines_test.py
+++ misttests/api/core/machines_test.py
@@ -131,7 +131,7 @@ def test_associate_key_no_api_token(pretty_print, mist_core):
 @pytest.mark.incremental
 class TestMachinesFunctionality:
 
-    def test_list_machines(self, pretty_print, mist_core, owner_api_token):
+    def test_list_machines(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.add_cloud(title='Docker', provider= 'docker', api_token=owner_api_token,
                                        docker_host=config.CREDENTIALS['DOCKER']['host'],
                                        docker_port=config.CREDENTIALS['DOCKER']['port'],
@@ -139,5 +139,8 @@ class TestMachinesFunctionality:
                                        ca_cert_file=config.CREDENTIALS['DOCKER']['ca'],
                                        key_file=config.CREDENTIALS['DOCKER']['key'],
                                        cert_file=config.CREDENTIALS['DOCKER']['cert']).post()
+        assert_response_ok(response)
+        cache.set('cloud_id', response.json()['id'])
+        response = mist_core.list_machines(cloud_id=cache.get('cloud_id', ''), api_token=owner_api_token).get()
         assert_response_ok(response)
         print "Success!!!"
