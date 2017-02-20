5e14e52d21d1eceed9cff5b68fd62a60edc9218b
spiros <spirostza77@hotmail.com>
2017-02-20 21:25:03 +0200

    remove delete_multiple_keys

--- misttests/api/core/key_test.py
+++ misttests/api/core/key_test.py
@@ -240,36 +240,36 @@ class TestSimpleUserKeyCycle:
                 assert key['isDefault'] == False, "More than one keys are set as default!"
         print "Success!!!"
 
-    def test_delete_multiple_keys(self, pretty_print, mist_core,
-                                  owner_api_token, private_key):
-        key_ids = []
-        # add 3 more keys and then delete them
-        for i in range(3):
-            response = mist_core.list_keys(api_token=owner_api_token).get()
-            assert_response_ok(response)
-            new_key_name = get_random_key_name(json.loads(response.content))
-            response = mist_core.add_key(name=new_key_name, private=private_key,
-                                         api_token=owner_api_token).put()
-            assert_response_ok(response)
-            response = mist_core.list_keys(api_token=owner_api_token).get()
-            assert_response_ok(response)
-            script = get_keys_with_id(new_key_name,
-                                      json.loads(response.content))
-            new_key_id = script[0]['id']
-            assert_list_not_empty(script, "Key was added but is not visible in"
-                                          " the list of keys")
-            key_ids.append(new_key_id)
-
-        key_ids.append('bla')
-        key_ids.append('bla2')
-
-        response = mist_core.delete_keys(key_ids=key_ids,
-                                         api_token=owner_api_token).delete()
-        assert_response_ok(response)
-        report = json.loads(response.content)
-        for key_id in key_ids:
-            if 'bla' not in key_id:
-                assert_equal(report.get(key_id, ''), 'deleted', report)
-            if 'bla' in key_id:
-                assert_equal(report.get(key_id, ''), 'not_found', report)
-        print "Success!!!"
+    # def test_delete_multiple_keys(self, pretty_print, mist_core,
+    #                               owner_api_token, private_key):
+    #     key_ids = []
+    #     # add 3 more keys and then delete them
+    #     for i in range(3):
+    #         response = mist_core.list_keys(api_token=owner_api_token).get()
+    #         assert_response_ok(response)
+    #         new_key_name = get_random_key_name(json.loads(response.content))
+    #         response = mist_core.add_key(name=new_key_name, private=private_key,
+    #                                      api_token=owner_api_token).put()
+    #         assert_response_ok(response)
+    #         response = mist_core.list_keys(api_token=owner_api_token).get()
+    #         assert_response_ok(response)
+    #         script = get_keys_with_id(new_key_name,
+    #                                   json.loads(response.content))
+    #         new_key_id = script[0]['id']
+    #         assert_list_not_empty(script, "Key was added but is not visible in"
+    #                                       " the list of keys")
+    #         key_ids.append(new_key_id)
+    #
+    #     key_ids.append('bla')
+    #     key_ids.append('bla2')
+    #
+    #     response = mist_core.delete_keys(key_ids=key_ids,
+    #                                      api_token=owner_api_token).delete()
+    #     assert_response_ok(response)
+    #     report = json.loads(response.content)
+    #     for key_id in key_ids:
+    #         if 'bla' not in key_id:
+    #             assert_equal(report.get(key_id, ''), 'deleted', report)
+    #         if 'bla' in key_id:
+    #             assert_equal(report.get(key_id, ''), 'not_found', report)
+    #     print "Success!!!"
