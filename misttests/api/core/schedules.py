88a411be3f00db63722882c49c47b4ea325f604d
spiros <spirostza77@hotmail.com>
2017-02-20 15:44:28 +0200

    list_schedules

--- misttests/api/core/schedules.py
+++ misttests/api/core/schedules.py
@@ -12,7 +12,19 @@ from misttests.api.helpers import *
 
 
 def test_list_schedules(pretty_print, mist_core, owner_api_token):
-    response = mist_core.list_schedules(api_token=owner_api_token).post()
+    response = mist_core.list_schedules(api_token=owner_api_token).get()
     assert_response_ok(response)
     assert len(response.json()) == 0
+    print "Success!!!"
+
+
+def test_list_schedules_no_api_token(pretty_print, mist_core):
+    response = mist_core.list_schedules(api_token='').get()
+    assert_response_unauthorized(response)
+    print "Success!!!"
+
+
+def test_list_schedules_wrong_api_token(pretty_print, mist_core):
+    response = mist_core.list_schedules(api_token='').get()
+    assert_response_unauthorized(response)
     print "Success!!!"
