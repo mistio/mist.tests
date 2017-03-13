3bdcfd7b1d1d40950aee2f72386c48d6c1067576
spiros <spirostza77@hotmail.com>
2017-03-13 19:26:00 +0200

    pep8 fix in selenium_utils.py

--- misttests/helpers/selenium_utils.py
+++ misttests/helpers/selenium_utils.py
@@ -4,8 +4,6 @@ from misttests import config
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
 from selenium.webdriver.remote.errorhandler import NoSuchWindowException
 
 import logging
