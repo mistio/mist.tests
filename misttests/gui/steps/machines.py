7d4dc2e1cb24499b77166a9734c9729e24b96cde
spiros <spirostza77@hotmail.com>
2017-03-14 00:48:22 +0200

    remove unused steps

--- misttests/gui/steps/machines.py
+++ misttests/gui/steps/machines.py
@@ -102,49 +102,6 @@ def assert_machine_added(context, name, seconds):
     assert False, u'%s is not added' % name
 
 
-# @step(u'"{name}" machine state should be "{state}" within {seconds} seconds')
-# def assert_machine_state(context, name, state, seconds):
-#     if context.mist_config.get(name):
-#         name = context.mist_config.get(name)
-#
-#     end_time = time() + int(seconds)
-#     while time() < end_time:
-#         machine = get_machine(context, name)
-#         if machine:
-#             try:
-#                 if state in safe_get_element_text(machine):
-#                     return
-#             except NoSuchElementException:
-#                 pass
-#             except StaleElementReferenceException:
-#                 pass
-#         sleep(2)
-#
-#     assert False, u'%s state is not "%s"' % (name, state)
-#
-#
-# @step(u'"{name}" machine should be probed within {seconds} seconds')
-# def assert_machine_probed(context, name, seconds):
-#     if context.mist_config.get(name):
-#         name = context.mist_config.get(name)
-#
-#     end_time = time() + int(seconds)
-#     while time() < end_time:
-#         machine = get_machine(context, name)
-#         if machine:
-#             try:
-#                 machine.find_element_by_class_name("probed")
-#                 return
-#             except NoSuchElementException:
-#                 pass
-#             except StaleElementReferenceException:
-#                 pass
-#             sleep(3)
-#
-#     assert False, u'%s machine is not probed within %s seconds' % (
-#                     name, seconds)
-
-
 def get_machine(context, name):
     try:
         placeholder = context.browser.find_element_by_tag_name("page-machines").find_element_by_id("items")
@@ -206,18 +163,6 @@ def wait_for_loader_to_finish(context, seconds):
     assert False, "Could not locate ajax loader"
 
 
-@step(u'If the key addition was successful')
-def success(context):
-    try:
-        popup = context.browser.find_element_by_id('machine-userPort-popup-popup')
-        div = popup.find_element_by_class_name('message')
-        div_text = safe_get_element_text(div)
-        if div_text == 'Cannot connect as root on port 22':
-            raise ValueError('Could not connect with server with ssh key')
-    except NoSuchElementException:
-        pass
-
-
 @step(u'probing was successful')
 def check_probing(context):
     rows = context.browser.find_elements_by_tag_name('tr')
@@ -231,98 +176,6 @@ def check_probing(context):
                                           "(message is: %s)" % cells_one_text
             return
     assert False, "Could not find any line about probing"
-
-
-@given(u'ssh key with name "{ssh_key_name}" is added')
-def ssh_key_is_added(context, ssh_key_name):
-    # first we have to find the keys button
-    buttons = context.browser.find_elements_by_class_name('ui-btn')
-    for button in buttons:
-        button_text = safe_get_element_text(button)
-        if 'add key' in button_text.lower():
-            # if there no keys then it will be called "Add key"
-            context.execute_steps(u"""
-                Then I click the button "Add key"
-                And I expect for "non-associated-keys-popup-popup" popup to appear within max 4 seconds
-            """)
-            # check if the key is already uploaded but not associated
-            key_already_associated = False
-            non_associated_keys = context.browser.find_element_by_id('non-associated-keys-popup').find_elements_by_tag_name('li')
-            for non_associated_key in non_associated_keys:
-                if context.mist_config['CREDENTIALS'][ssh_key_name]['key_name'].lower() in safe_get_element_text(non_associated_key).lower():
-                    non_associated_key.click()
-                    key_already_associated = True
-                    break
-
-            if not key_already_associated:
-                context.execute_steps(u"""
-                    When I click the "New key" button inside the "Add key" popup
-                    Then I expect for "key-add-popup" popup to appear within max 2 seconds
-                    And I upload the ssh key with name "%s"
-                """ % ssh_key_name)
-
-            context.execute_steps(u"""
-                Then I expect for "machine-keys-panel" side panel to appear within max 4 seconds
-                And I expect for "machine-associating-key-loader" loader to finish within max 100 seconds
-                Then If the key addition was successful
-            """)
-            context.browser.find_elements_by_class_name('ui-panel-dismiss')[0].click()
-            return
-        elif 'keys' in button_text.lower():
-            # otherwise it will be called "? keys" where ? is the number of
-            # saved keys. before adding the key we need to check if it's already
-            # saved
-            context.execute_steps(u'''
-                Then I click the button "%s"
-                And I expect for "machine-keys-panel" side panel to appear within max 4 seconds
-            ''' % safe_get_element_text(button))
-            machine_keys_list = context.browser.find_element_by_id("machine-keys")
-            machines_keys = machine_keys_list.find_elements_by_class_name(
-                "small-list-item")
-            checked_texts = []
-            for machines_key in machines_keys:
-                machines_key_text = safe_get_element_text(machines_key)
-                if not machines_key_text or not machines_key_text.strip():
-                    # sometimes the code checks for the texts too fast and they
-                    # haven't been fetched yet so we do a sleep
-                    sleep(1)
-                checked_texts.append(machines_key_text)
-                if context.mist_config['CREDENTIALS'][ssh_key_name]['key_name']\
-                        in machines_key_text:
-                    context.browser.find_elements_by_class_name('ui-panel-dismiss')[0].click()
-                    context.execute_steps(u'Then I expect for '
-                                          u'"machine-keys-panel" side panel '
-                                          u'to disappear within max 4 seconds')
-                    return
-            context.execute_steps(u"""
-                When I click the "New key" button inside the "Manage Keys" panel
-                And I expect for "non-associated-keys-popup" popup to appear within max 4 seconds
-            """)
-            # check if the key is already uploaded but not associated
-            key_already_associated = False
-            non_associated_keys = context.browser.find_element_by_id('non-associated-keys-popup').find_elements_by_tag_name('li')
-            for non_associated_key in non_associated_keys:
-                if context.mist_config['CREDENTIALS'][ssh_key_name]['key_name'].lower() in safe_get_element_text(non_associated_key).lower():
-                    non_associated_key.click()
-                    key_already_associated = True
-                    break
-
-            if not key_already_associated:
-                context.execute_steps(u"""
-                    When I click the "New key" button inside the "Add Key" popup
-                    Then I expect for "key-add-popup" popup to appear within max 2 seconds
-                    And I upload the ssh key with name "%s"
-                """ % ssh_key_name)
-
-            context.execute_steps(u"""
-                Then I expect for "key-generate-loader" loader to finish within max 5 seconds
-                And I expect for "machine-associating-key-loader" loader to finish within max 100 seconds
-                And If the key addition was successful
-            """)
-            context.browser.find_elements_by_class_name('ui-panel-dismiss')[0].click()
-            context.execute_steps(u'Then I expect for "machine-keys-panel" '
-                                  u'side panel to disappear within max 4 '
-                                  u'seconds')
 
 
 @step(u'I fill "{value}" as rule value')
@@ -367,11 +220,13 @@ def click_rule_dropdown(context, rule_class):
     rule_element = context.browser.find_element_by_xpath('//paper-dropdown-menu[contains(@class, "%s")]' % rule_class)
     clicketi_click(context, rule_element)
 
+
 @step(u'I save the rule')
 def save_rule(context):
     container = context.browser.find_element_by_xpath('//div[contains(@class, "rule-actions")]')
     button = container.find_element_by_xpath('.//paper-button[contains(@class, "blue")]')
     clicketi_click(context, button)
+
 
 @step(u'I remove previous rules')
 def remove_previous_rules(context):
