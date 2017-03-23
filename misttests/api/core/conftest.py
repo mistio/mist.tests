f0ef42a0f04950ede7be5a18cb085d94c42049c8
spiros <spirostza77@hotmail.com>
2017-03-23 15:22:01 +0200

    core/conftest cleanup done

--- misttests/api/core/conftest.py
+++ misttests/api/core/conftest.py
@@ -1,36 +1,9 @@
-from .core import MistCoreApi
-
 from misttests.api.io.conftest import *
-from misttests.helpers.setup import setup_user_if_not_exists
 
 
 @pytest.fixture(scope='module')
 def valid_api_token(request):
     return common_valid_api_token(request, email=email(), password=password1())
-
-
-@pytest.fixture(scope='module')
-def owner_api_token(request):
-    _mist_core = mist_core()
-    email = owner_email()
-    password = owner_password()
-    setup_user_if_not_exists(email, password)
-    _mist_core.login(email, password)
-    personal_api_token = common_valid_api_token(request,
-                                                email=email,
-                                                password=password)
-    response = _mist_core.list_orgs(api_token=personal_api_token).get()
-    assert_response_ok(response)
-    org_id = None
-    org = response.json()
-
-    org_id = org[0]['id']
-    assert_is_not_none(org_id)
-
-    return common_valid_api_token(request,
-                                  email=email,
-                                  password=password,
-                                  org_id=org_id)
 
 
 @pytest.fixture(scope='module')
@@ -41,7 +14,6 @@ def member1_api_token(request):
     personal_api_token = common_valid_api_token(request,
                                                 email=email,
                                                 password=password)
-
     _org_name = org_name()
     response = _mist_core.list_orgs(api_token=personal_api_token).get()
     assert_response_ok(response)
@@ -66,7 +38,6 @@ def member2_api_token(request):
     personal_api_token = common_valid_api_token(request,
                                                 email=email,
                                                 password=password)
-
     _org_name = org_name()
     response = _mist_core.list_orgs(api_token=personal_api_token).get()
     assert_response_ok(response)
