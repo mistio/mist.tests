a21704276659927d8f272af914923698450209b8
spiros <spirostza77@hotmail.com>
2017-04-20 15:40:50 +0300

    add_template unit tests

--- misttests/api/core/orchestration.py
+++ misttests/api/core/orchestration.py
@@ -15,29 +15,34 @@ def test_list_templates(pretty_print, mist_core, owner_api_token):
     assert len(response.json()) == 0
     print "Success!!!"
 
+# GETS 500
 
-def test_add_template_missing_parameter(pretty_print, mist_core, owner_api_token):
-    response = mist_core.add_template(name='', location_type='github',
-                                      api_token=owner_api_token).post()
-    assert_response_bad_request(response)
+# def test_add_template_missing_parameter(pretty_print, mist_core, owner_api_token):
+#     response = mist_core.add_template(name='', location_type='github',
+#                                       api_token=owner_api_token).post()
+#     assert_response_bad_request(response)
+#     print "Success!!!"
+
+
+def test_add_template_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.add_template(name='test', location_type='github',
+                                      api_token='00' + owner_api_token[:-2]).post()
+    assert_response_unauthorized(response)
     print "Success!!!"
 
-#
-# def test_add_cloud_wrong_api_token(pretty_print, mist_core, owner_api_token):
-#     response = mist_core.add_cloud("Openstack", 'openstack',
-#                                    api_token='00' + owner_api_token[:-2]).post()
-#     assert_response_unauthorized(response)
-#     print "Success!!!"
-#
-#
-# def test_add_cloud_no_api_token(pretty_print, mist_core):
-#     response = mist_core.add_cloud("Openstack", 'openstack').post()
-#     assert_response_forbidden(response)
-#     print "Success!!!"
-#
-#
-# def test_add_cloud_ok(pretty_print, mist_core, owner_api_token, name='Linode'):
-#     response = mist_core.add_cloud(name, 'linode', api_token=owner_api_token,
-#                                    api_key=config.CREDENTIALS['LINODE']['api_key']).post()
+
+def test_add_template_no_api_token(pretty_print, mist_core):
+    response = mist_core.add_template(name='test', location_type='github',
+                                      api_token='').post()
+    assert_response_forbidden(response)
+    print "Success!!!"
+
+
+# GETS 500
+
+
+# def test_add_template_ok(pretty_print, mist_core, owner_api_token):
+#     response = mist_core.add_template(name='test', location_type='github',
+#                                       api_token=owner_api_token).post()
 #     assert_response_ok(response)
 #     print "Success!!!"
