b42b2b3009195a81cd9be1845447283340afff38
spiros <spirostza77@hotmail.com>
2017-04-20 16:19:47 +0300

    delete template unit-tests

--- misttests/api/core/orchestration.py
+++ misttests/api/core/orchestration.py
@@ -16,7 +16,6 @@ def test_list_templates(pretty_print, mist_core, owner_api_token):
     print "Success!!!"
 
 # GETS 500
-
 # def test_add_template_missing_parameter(pretty_print, mist_core, owner_api_token):
 #     response = mist_core.add_template(name='', location_type='github',
 #                                       api_token=owner_api_token).post()
@@ -39,8 +38,6 @@ def test_add_template_no_api_token(pretty_print, mist_core):
 
 
 # GETS 500
-
-
 # def test_add_template_ok(pretty_print, mist_core, owner_api_token):
 #     response = mist_core.add_template(name='test', location_type='github',
 #                                       api_token=owner_api_token).post()
@@ -60,17 +57,35 @@ def test_edit_template_no_api_token(pretty_print, mist_core):
                                        api_token='').put()
     assert_response_forbidden(response)
     print "Success!!!"
-#
-#
-# def test_rename_cloud_no_api_token(pretty_print, mist_core, owner_api_token):
-#     response = mist_core.rename_cloud(cloud_id='dummy', new_name='test',
-#                                       api_token='00' + owner_api_token[:-2]).put()
-#     assert_response_unauthorized(response)
-#     print "Success!!!"
-#
-#
-# def test_rename_cloud_missing_parameter(pretty_print, mist_core, owner_api_token):
-#     response = mist_core.rename_cloud(cloud_id='dummy', new_name='',
-#                                       api_token=owner_api_token).put()
-#     assert_response_not_found(response)
-#     print "Success!!!"
+
+
+def test_edit_template_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.edit_template(template_id='dummy', name='test',
+                                       api_token='00' + owner_api_token[:-2]).put()
+    assert_response_unauthorized(response)
+    print "Success!!!"
+
+
+def test_edit_template_missing_parameter(pretty_print, mist_core, owner_api_token):
+    response = mist_core.edit_template(template_id='dummy', name='',
+                                       api_token=owner_api_token).put()
+    assert_response_not_found(response)
+    print "Success!!!"
+
+
+def test_delete_template_wrong_id(pretty_print, mist_core, owner_api_token):
+    response = mist_core.delete_template(template_id='dummy', api_token=owner_api_token).delete()
+    assert_response_not_found(response)
+    print "Success"
+
+
+def test_delete_template_no_api_token(pretty_print, mist_core):
+    response = mist_core.delete_template(template_id='dummy', api_token='').delete()
+    assert_response_forbidden(response)
+    print "Success!!!"
+
+
+def test_delete_template_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.delete_template(template_id='dummy', api_token='00' + owner_api_token[:-2]).delete()
+    assert_response_unauthorized(response)
+    print "Success!!!"
