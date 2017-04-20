f4c841fae31e1a6bc772ed9635bbed759e12df0c
spiros <spirostza77@hotmail.com>
2017-04-20 16:11:39 +0300

    edit_template unit tests

--- misttests/api/core/orchestration.py
+++ misttests/api/core/orchestration.py
@@ -46,3 +46,31 @@ def test_add_template_no_api_token(pretty_print, mist_core):
 #                                       api_token=owner_api_token).post()
 #     assert_response_ok(response)
 #     print "Success!!!"
+
+
+def test_edit_template_wrong_id(pretty_print, mist_core, owner_api_token):
+    response = mist_core.edit_template(template_id='dummy', name='test',
+                                       api_token=owner_api_token).put()
+    assert_response_not_found(response)
+    print "Success!!!"
+
+
+def test_edit_template_no_api_token(pretty_print, mist_core):
+    response = mist_core.edit_template(template_id='dummy', name='test',
+                                       api_token='').put()
+    assert_response_forbidden(response)
+    print "Success!!!"
+#
+#
+# def test_rename_cloud_no_api_token(pretty_print, mist_core, owner_api_token):
+#     response = mist_core.rename_cloud(cloud_id='dummy', new_name='test',
+#                                       api_token='00' + owner_api_token[:-2]).put()
+#     assert_response_unauthorized(response)
+#     print "Success!!!"
+#
+#
+# def test_rename_cloud_missing_parameter(pretty_print, mist_core, owner_api_token):
+#     response = mist_core.rename_cloud(cloud_id='dummy', new_name='',
+#                                       api_token=owner_api_token).put()
+#     assert_response_not_found(response)
+#     print "Success!!!"
