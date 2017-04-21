f8ff605dcab674c5e1d565bf815d3375d4ec2cde
spiros <spirostza77@hotmail.com>
2017-04-21 19:10:59 +0300

    add template bad requests done

--- misttests/api/core/orchestration.py
+++ misttests/api/core/orchestration.py
@@ -206,10 +206,16 @@ class TestOrchestrationFunctionality:
                                           template_github='',
                                           entrypoint='blueprint.yaml').post()
         assert_response_bad_request(response)
+        response = mist_core.add_template(api_token=owner_api_token, name='Template1', location_type='url',
+                                          template_github='https://github.com/mistio/kubernetes-blueprint',
+                                          entrypoint='blueprint.yaml').post()
+        assert_response_bad_request(response)
+        print "Success!!!"
 
     def test_add_template(self, pretty_print, mist_core, owner_api_token):
         response = mist_core.add_template(api_token=owner_api_token, name='Template1', location_type='github',
-                                          template_github='https://github.com/mistio/kubernetes-blueprint', entrypoint="blueprint.yaml").post()
+                                          template_github='https://github.com/mistio/kubernetes-blueprint',
+                                          entrypoint="blueprint.yaml").post()
         assert_response_ok(response)
         response = mist_core.list_templates(api_token=owner_api_token).get()
         assert len(response.json()) == 1, "Although template has been added, it is not visible in list_templates"
