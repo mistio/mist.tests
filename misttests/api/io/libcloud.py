056a8270418383f4e106bec3609d0ee3462dba47
Mike Muzurakis <clopy@mist.io>
2017-04-25 18:24:11 +0300

    add debugging code

--- misttests/api/io/libcloud.py
+++ misttests/api/io/libcloud.py
@@ -39,6 +39,7 @@ class TestMachinesFunctionality:
                 assert_response_ok(response)
                 cache.set('cloud_id', response.json()['id'])
                 response = mist_core.list_machines(cloud_id=cache.get('cloud_id', ''), api_token=owner_api_token).get()
+                print(response)
                 assert_response_ok(response)
-                assert len(response.json()) > 0, "List machines did not return any machines"
+                assert len(response.json()) >= 0, "List machines did not return a proper result"
         print "Success!!!"
