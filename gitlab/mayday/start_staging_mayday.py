d18d3ee184aac3c09381b1b18502186ed57f5b7e
spiros <spirostza77@hotmail.com>
2017-05-02 19:19:28 +0300

    update start_staging_mayday with correct token and project id

--- gitlab/mayday/start_staging_mayday.py
+++ gitlab/mayday/start_staging_mayday.py
@@ -1,8 +1,9 @@
 import requests
 
-gl_url = "https://gitlab.ops.mist.io/api/v3/projects/29/trigger/builds"
-TOKEN = 'ededbd99791f66c6a958b70c8689d7'
-data = {"token": TOKEN, "ref": 'improve_mayday_tests'}
+
+gl_url = "https://gitlab.ops.mist.io/api/v3/projects/50/trigger/builds"
+TOKEN = '170af2ffaf5fd3edb8dbf1022a4f8c'
+data = {"token": TOKEN, "ref": 'check_triggering'}
 
 request = requests.post(gl_url, data=data)
 data = request.json()
