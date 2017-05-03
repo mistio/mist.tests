5c33b1c266a2eb1c36a398353d752a03ca235e1f
spiros <spirostza77@hotmail.com>
2017-05-03 20:25:11 +0300

    test everything

--- misttests/api/io/keys.py
+++ misttests/api/io/keys.py
@@ -1,20 +1,20 @@
 import pytest
 
 from misttests.api.helpers import *
-
-
-############################################################################
-#                             Unit Testing                                 #
-############################################################################
-
-
+#
+#
+# ############################################################################
+# #                             Unit Testing                                 #
+# ############################################################################
+#
+#
 def test_list_keys(pretty_print, mist_core, owner_api_token):
     response = mist_core.list_keys(api_token=owner_api_token).get()
     assert_response_ok(response)
     assert len(response.json()) == 0
     print "Success!!!"
-
-
+#
+#
 # # # TODO: check below... csrf=None is ok but csrf='' is not?
 # # def test_generate_key_wrong_api_token(pretty_print, mist_core):
 # #     response = mist_core.generate_keypair(csrf_token='').post()
@@ -126,150 +126,150 @@ def test_set_default_key_wrong_id(pretty_print, cache, mist_core,
         api_token=owner_api_token).post()
     assert_response_not_found(response)
     print "Success!!!"
-
-
-############################################################################
-#                          Functional Testing                              #
-############################################################################
-
-
-@pytest.mark.incremental
-class TestSimpleUserKeyCycle:
-    def test_add_key(self, pretty_print, cache, mist_core, owner_api_token,
-                     private_key):
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) == 0
-        response = mist_core.add_key(
-            name='TestKey',
-            private=private_key,
-            api_token=owner_api_token).put()
-        assert_response_ok(response)
-        cache.set('key_id', response.json()['id'])
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) == 1
-        print "Success!!!"
-
-    def test_add_key_duplicate_name(self, pretty_print, cache, mist_core,
-                                    owner_api_token, private_key):
-        response = mist_core.add_key(
-            name='TestKey',
-            private=private_key,
-            api_token=owner_api_token).put()
-        assert_response_conflict(response)
-        print "Success!!!"
-
-    def test_rename_key(self, pretty_print, cache, mist_core,
-                        owner_api_token):
-        response = mist_core.edit_key(
-                    id=cache.get('key_id', ''),
-                    new_name='Key1',
-                    api_token=owner_api_token).put()
-        assert_response_ok(response)
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert 'Key1' == response.json()[0]['name'], "Although response was 200, key was not renamed"
-        print "Success"
-
-    def test_rename_key_no_name(self, pretty_print, cache, mist_core,
-                                owner_api_token):
-        response = mist_core.edit_key(
-                    id=cache.get('key_id', ''),
-                    new_name='',
-                    api_token=owner_api_token).put()
-        assert_response_bad_request(response)
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert 'Key1' == response.json()[0]['name']
-        print "Success"
-
-    def test_generate_key(self, pretty_print, cache, mist_core, owner_api_token):
-        response = mist_core.generate_keypair(api_token=owner_api_token).post()
-        assert_response_ok(response)
-        if 'public' not in response.json().keys() or 'priv' not in response.json().keys():
-            assert False, "Public key was not generated!"
-        cache.set('key_priv', response.json()['priv'])
-        response = mist_core.add_key(
-            name='Key2',
-            private=cache.get('key_priv', ''),
-            api_token=owner_api_token).put()
-        assert_response_ok(response)
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) == 2
-        print "Success!!!"
-
-    def test_get_private_key(self, pretty_print, private_key, cache, mist_core, owner_api_token):
-        response = mist_core.get_private_key(key_id=cache.get('key_id', ''),
-                                             api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert response.json() == private_key
-        print "Success!!!"
-
-    def test_delete_key(self, pretty_print, cache, mist_core, owner_api_token):
-        response = mist_core.delete_key(key_id=cache.get('key_id', ''),
-                                        api_token=owner_api_token).delete()
-        assert_response_ok(response)
-        response = mist_core.delete_key(key_id=cache.get('key_id', ''),
-                                        api_token=owner_api_token).delete()
-        assert_response_not_found(response)
-        print "Success"
-
-    def test_set_default_key(self, pretty_print, cache, mist_core, owner_api_token,
-                             private_key):
-        response = mist_core.add_key(
-            name='TestKey2',
-            private=private_key,
-            api_token=owner_api_token).put()
-        assert_response_ok(response)
-        cache.set('key_id', response.json()['id'])
-        assert response.json()['isDefault'] == False, "Key was added as default although it shouldn't be!"
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) == 2
-        response = mist_core.set_default_key(
-            key_id=cache.get('key_id', ''),
-            api_token=owner_api_token).post()
-        assert_response_ok(response)
-        response = mist_core.list_keys(api_token=owner_api_token).get()
-        for key in response.json():
-            if key['id'] == cache.get('key_id', ''):
-                assert key['isDefault'], "Key is not default although response was 200"
-            else:
-                assert key['isDefault'] == False, "More than one keys are set as default!"
-        print "Success!!!"
-
-    # def test_delete_multiple_keys(self, pretty_print, mist_core,
-    #                               owner_api_token, private_key):
-    #     key_ids = []
-    #     # add 3 more keys and then delete them
-    #     for i in range(3):
-    #         response = mist_core.list_keys(api_token=owner_api_token).get()
-    #         assert_response_ok(response)
-    #         new_key_name = get_random_key_name(json.loads(response.content))
-    #         response = mist_core.add_key(name=new_key_name, private=private_key,
-    #                                      api_token=owner_api_token).put()
-    #         assert_response_ok(response)
-    #         response = mist_core.list_keys(api_token=owner_api_token).get()
-    #         assert_response_ok(response)
-    #         script = get_keys_with_id(new_key_name,
-    #                                   json.loads(response.content))
-    #         new_key_id = script[0]['id']
-    #         assert_list_not_empty(script, "Key was added but is not visible in"
-    #                                       " the list of keys")
-    #         key_ids.append(new_key_id)
-    #
-    #     key_ids.append('bla')
-    #     key_ids.append('bla2')
-    #
-    #     response = mist_core.delete_keys(key_ids=key_ids,
-    #                                      api_token=owner_api_token).delete()
-    #     assert_response_ok(response)
-    #     report = json.loads(response.content)
-    #     for key_id in key_ids:
-    #         if 'bla' not in key_id:
-    #             assert_equal(report.get(key_id, ''), 'deleted', report)
-    #         if 'bla' in key_id:
-    #             assert_equal(report.get(key_id, ''), 'not_found', report)
-    #     print "Success!!!"
+#
+#
+# ############################################################################
+# #                          Functional Testing                              #
+# ############################################################################
+#
+#
+# @pytest.mark.incremental
+# class TestSimpleUserKeyCycle:
+#     def test_add_key(self, pretty_print, cache, mist_core, owner_api_token,
+#                      private_key):
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert len(response.json()) == 0
+#         response = mist_core.add_key(
+#             name='TestKey',
+#             private=private_key,
+#             api_token=owner_api_token).put()
+#         assert_response_ok(response)
+#         cache.set('key_id', response.json()['id'])
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert len(response.json()) == 1
+#         print "Success!!!"
+#
+#     def test_add_key_duplicate_name(self, pretty_print, cache, mist_core,
+#                                     owner_api_token, private_key):
+#         response = mist_core.add_key(
+#             name='TestKey',
+#             private=private_key,
+#             api_token=owner_api_token).put()
+#         assert_response_conflict(response)
+#         print "Success!!!"
+#
+#     def test_rename_key(self, pretty_print, cache, mist_core,
+#                         owner_api_token):
+#         response = mist_core.edit_key(
+#                     id=cache.get('key_id', ''),
+#                     new_name='Key1',
+#                     api_token=owner_api_token).put()
+#         assert_response_ok(response)
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert 'Key1' == response.json()[0]['name'], "Although response was 200, key was not renamed"
+#         print "Success"
+#
+#     def test_rename_key_no_name(self, pretty_print, cache, mist_core,
+#                                 owner_api_token):
+#         response = mist_core.edit_key(
+#                     id=cache.get('key_id', ''),
+#                     new_name='',
+#                     api_token=owner_api_token).put()
+#         assert_response_bad_request(response)
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert 'Key1' == response.json()[0]['name']
+#         print "Success"
+#
+#     def test_generate_key(self, pretty_print, cache, mist_core, owner_api_token):
+#         response = mist_core.generate_keypair(api_token=owner_api_token).post()
+#         assert_response_ok(response)
+#         if 'public' not in response.json().keys() or 'priv' not in response.json().keys():
+#             assert False, "Public key was not generated!"
+#         cache.set('key_priv', response.json()['priv'])
+#         response = mist_core.add_key(
+#             name='Key2',
+#             private=cache.get('key_priv', ''),
+#             api_token=owner_api_token).put()
+#         assert_response_ok(response)
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert len(response.json()) == 2
+#         print "Success!!!"
+#
+#     def test_get_private_key(self, pretty_print, private_key, cache, mist_core, owner_api_token):
+#         response = mist_core.get_private_key(key_id=cache.get('key_id', ''),
+#                                              api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert response.json() == private_key
+#         print "Success!!!"
+#
+#     def test_delete_key(self, pretty_print, cache, mist_core, owner_api_token):
+#         response = mist_core.delete_key(key_id=cache.get('key_id', ''),
+#                                         api_token=owner_api_token).delete()
+#         assert_response_ok(response)
+#         response = mist_core.delete_key(key_id=cache.get('key_id', ''),
+#                                         api_token=owner_api_token).delete()
+#         assert_response_not_found(response)
+#         print "Success"
+#
+#     def test_set_default_key(self, pretty_print, cache, mist_core, owner_api_token,
+#                              private_key):
+#         response = mist_core.add_key(
+#             name='TestKey2',
+#             private=private_key,
+#             api_token=owner_api_token).put()
+#         assert_response_ok(response)
+#         cache.set('key_id', response.json()['id'])
+#         assert response.json()['isDefault'] == False, "Key was added as default although it shouldn't be!"
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         assert_response_ok(response)
+#         assert len(response.json()) == 2
+#         response = mist_core.set_default_key(
+#             key_id=cache.get('key_id', ''),
+#             api_token=owner_api_token).post()
+#         assert_response_ok(response)
+#         response = mist_core.list_keys(api_token=owner_api_token).get()
+#         for key in response.json():
+#             if key['id'] == cache.get('key_id', ''):
+#                 assert key['isDefault'], "Key is not default although response was 200"
+#             else:
+#                 assert key['isDefault'] == False, "More than one keys are set as default!"
+#         print "Success!!!"
+#
+#     # def test_delete_multiple_keys(self, pretty_print, mist_core,
+#     #                               owner_api_token, private_key):
+#     #     key_ids = []
+#     #     # add 3 more keys and then delete them
+#     #     for i in range(3):
+#     #         response = mist_core.list_keys(api_token=owner_api_token).get()
+#     #         assert_response_ok(response)
+#     #         new_key_name = get_random_key_name(json.loads(response.content))
+#     #         response = mist_core.add_key(name=new_key_name, private=private_key,
+#     #                                      api_token=owner_api_token).put()
+#     #         assert_response_ok(response)
+#     #         response = mist_core.list_keys(api_token=owner_api_token).get()
+#     #         assert_response_ok(response)
+#     #         script = get_keys_with_id(new_key_name,
+#     #                                   json.loads(response.content))
+#     #         new_key_id = script[0]['id']
+#     #         assert_list_not_empty(script, "Key was added but is not visible in"
+#     #                                       " the list of keys")
+#     #         key_ids.append(new_key_id)
+#     #
+#     #     key_ids.append('bla')
+#     #     key_ids.append('bla2')
+#     #
+#     #     response = mist_core.delete_keys(key_ids=key_ids,
+#     #                                      api_token=owner_api_token).delete()
+#     #     assert_response_ok(response)
+#     #     report = json.loads(response.content)
+#     #     for key_id in key_ids:
+#     #         if 'bla' not in key_id:
+#     #             assert_equal(report.get(key_id, ''), 'deleted', report)
+#     #         if 'bla' in key_id:
+#     #             assert_equal(report.get(key_id, ''), 'not_found', report)
+#     #     print "Success!!!"
--- misttests/gui/core/mayday/features/mayday.feature
+++ misttests/gui/core/mayday/features/mayday.feature
@@ -20,60 +20,60 @@ Feature: Production
     Then "entropy" graph should have some values
     And I delete the "entropy" graph
 #
-#  @alert
-#  Scenario: Production rule and alert testing
-#    Given I am logged in to mist.core
-#    When I visit the Machines page after the counter has loaded
-#    Then I search for the mayday machine
-#    When I click the mayday machine
-#    And I clear the machines search bar
-#    And I expect the "machine" edit form to be visible within max 5 seconds
-#    Then I wait for the graphs to appear
-#    When I remove previous rules
-#    When I delete old mayday emails
-#    And I wait for 2 seconds
-#    And I focus on the "add new rule" button
-#    And I click the button "add new rule"
-#    Then I expect for "newrule" to be visible within max 20 seconds
-#    And I click the "metricName" rule
-##    And I click the "RAM" button in the dropdown with id "metricName"
-#    When I fill "0" as metric value
-#    And I save the rule
-#    When I remove previous rules
-#
-#  @ssh
-#  Scenario: Production ssh testing
-#    Given I am logged in to mist.core
-#    When I visit the Machines page after the counter has loaded
-#    Then I search for the mayday machine
-#    When I click the mayday machine
-#    And I clear the machines search bar
-#    And I expect the "machine" edit form to be visible within max 5 seconds
-#    Then I wait for the graphs to appear
-#    When I test the ssh connection 2 times for max 100 seconds each time
-#
-#  @celery
-#  Scenario: Production machine reboot testing
-#    Given I am logged in to mist.core
-#    When I visit the Machines page after the counter has loaded
-#    Then I search for the mayday machine
-#    And I open the actions dialog
-#    Then I expect for "select-action" modal to appear within max 4 seconds
-#    When I click the "Reboot" button inside the "select-action" modal
-#    Then I expect for "confirmation" modal to appear within max 4 seconds
-#    And I click the button "Reboot"
-#    Then I expect for "select-action" modal to disappear within max 4 seconds
-#    And I wait for 20 seconds
-#    And I open the actions dialog
-#    Then I expect for "select-action" modal to appear within max 4 seconds
-#    When I click the "Shell" button inside the "select-action" modal
-#    And I expect terminal to open within 3 seconds
-#    And I wait for 5 seconds
-#    And I type in the terminal "uptime"
-#    And I wait for 1 seconds
-#    Then up 0 min should be included in the output
-#    And I close the terminal
-#    And I wait for 1 seconds
+  @alert
+  Scenario: Production rule and alert testing
+    Given I am logged in to mist.core
+    When I visit the Machines page after the counter has loaded
+    Then I search for the mayday machine
+    When I click the mayday machine
+    And I clear the machines search bar
+    And I expect the "machine" edit form to be visible within max 5 seconds
+    Then I wait for the graphs to appear
+    When I remove previous rules
+    When I delete old mayday emails
+    And I wait for 2 seconds
+    And I focus on the "add new rule" button
+    And I click the button "add new rule"
+    Then I expect for "newrule" to be visible within max 20 seconds
+    And I click the "metricName" rule
+#    And I click the "RAM" button in the dropdown with id "metricName"
+    When I fill "0" as metric value
+    And I save the rule
+    When I remove previous rules
+
+  @ssh
+  Scenario: Production ssh testing
+    Given I am logged in to mist.core
+    When I visit the Machines page after the counter has loaded
+    Then I search for the mayday machine
+    When I click the mayday machine
+    And I clear the machines search bar
+    And I expect the "machine" edit form to be visible within max 5 seconds
+    Then I wait for the graphs to appear
+    When I test the ssh connection 2 times for max 100 seconds each time
+
+  @celery
+  Scenario: Production machine reboot testing
+    Given I am logged in to mist.core
+    When I visit the Machines page after the counter has loaded
+    Then I search for the mayday machine
+    And I open the actions dialog
+    Then I expect for "select-action" modal to appear within max 4 seconds
+    When I click the "Reboot" button inside the "select-action" modal
+    Then I expect for "confirmation" modal to appear within max 4 seconds
+    And I click the button "Reboot"
+    Then I expect for "select-action" modal to disappear within max 4 seconds
+    And I wait for 20 seconds
+    And I open the actions dialog
+    Then I expect for "select-action" modal to appear within max 4 seconds
+    When I click the "Shell" button inside the "select-action" modal
+    And I expect terminal to open within 3 seconds
+    And I wait for 5 seconds
+    And I type in the terminal "uptime"
+    And I wait for 1 seconds
+    Then up 0 min should be included in the output
+    And I close the terminal
+    And I wait for 1 seconds
 #
 ##  @github_sso_signin
 ##  Scenario: Sign in testing with github
@@ -100,10 +100,10 @@ Feature: Production
 ##    And I press the button with id "signIn"
 ##    When I wait for the dashboard to load
 ##    Then I logout
-#
-#  @confirm_alert_email
-#  Scenario: Confirm that alert email arrived
-#    Then I should receive an email within 200 seconds
+
+  @confirm_alert_email
+  Scenario: Confirm that alert email arrived
+    Then I should receive an email within 200 seconds
 
 
 # Fix step 'Then "entropy" graph should have some values ' (Cannot read property '0' of undefined error...)
