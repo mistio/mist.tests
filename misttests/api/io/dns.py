783bfa029e5bcfdc047fc2a4bb4f91500ad82a59
Kostis Tsaprailis <kostis.tsaprailis@gmail.com>
2017-05-09 15:58:46 +0300

    adding hardcoded region for AWS creds

--- misttests/api/io/dns.py
+++ misttests/api/io/dns.py
@@ -14,7 +14,7 @@ def test_list_zones(pretty_print, mist_core, cache,  owner_api_token):
     response = mist_core.add_cloud('EC2', 'ec2', api_token=owner_api_token,
                                    api_key=config.CREDENTIALS['AWS']['api_key'],
                                    api_secret=config.CREDENTIALS['AWS']['api_secret'],
-                                   region=config.CREDENTIALS['AWS']['region']).post()
+                                   region='ap-northeast-1').post()
     assert_response_ok(response)
     response = mist_core.list_clouds(api_token=owner_api_token).get()
     assert_response_ok(response)
