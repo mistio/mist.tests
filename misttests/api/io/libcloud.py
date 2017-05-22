5209d3f0e9b647a34fb545c3f37bf8fee1b25c86
spiros <spirostza77@hotmail.com>
2017-05-22 22:15:40 +0300

    pep8 fixes

--- misttests/api/io/libcloud.py
+++ misttests/api/io/libcloud.py
@@ -1,8 +1,7 @@
 from misttests.api.helpers import *
-from misttests import config
+from misttests.config import get_var_from_vault
 
 import pytest
-import json
 
 ############################################################################
 #                         Functional Testing                               #
@@ -14,19 +13,18 @@ class TestLibcloudFunctionality:
 
     def test_list_machines_docker(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.add_cloud(title='Docker', provider= 'docker', api_token=owner_api_token,
-                                       docker_host=config.CREDENTIALS['DOCKER']['host'],
-                                       docker_port=config.CREDENTIALS['DOCKER']['port'],
-                                       authentication=config.CREDENTIALS['DOCKER']['authentication'],
-                                       ca_cert_file=config.CREDENTIALS['DOCKER']['ca'],
-                                       key_file=config.CREDENTIALS['DOCKER']['key'],
-                                       cert_file=config.CREDENTIALS['DOCKER']['cert']).post()
+                                       docker_host=get_var_from_vault('clouds/docker', 'host'),
+                                       docker_port=get_var_from_vault('clouds/docker_orchestrator', 'port'),
+                                       authentication=get_var_from_vault('clouds/docker', 'authentication'),
+                                       ca_cert_file=get_var_from_vault('clouds/docker', 'ca'),
+                                       key_file=get_var_from_vault('clouds/docker', 'key'),
+                                       cert_file=get_var_from_vault('clouds/docker', 'cert')).post()
         assert_response_ok(response)
         cache.set('docker_cloud_id', response.json()['id'])
         response = mist_core.list_machines(cloud_id=cache.get('docker_cloud_id', ''), api_token=owner_api_token).get()
         assert_response_ok(response)
         assert len(response.json()) > 0, "List Docker machines did not return any machines"
         print "Success!!!"
-
 
     def test_list_machines_linode(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.add_cloud(title='Linode', provider= 'linode', api_token=owner_api_token,
