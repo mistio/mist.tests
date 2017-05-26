417be67119b320b32ec83358f84259dffbb01bfc
spiros <spirostza77@hotmail.com>
2017-05-26 21:05:46 +0300

    do not call lower() on gtraph title

--- misttests/gui/steps/graphs.py
+++ misttests/gui/steps/graphs.py
@@ -90,9 +90,7 @@ def wait_for_graph_to_appear(context, graph_title, seconds):
 
 @step(u'"{graph_title}" graph should have some values')
 def graph_some_value(context, graph_title):
-    graph_title = graph_title.lower()
     graph_xpath = '[id^="%s-"]' % graph_title
-
     try:
         datapoints = context.browser.execute_script("var graph = document.querySelector('%s'); return graph.data.datasets[0].data.length" % graph_xpath)
         if datapoints > 1:
