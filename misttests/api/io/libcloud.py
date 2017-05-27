8a45e3b4873711f979298741c50536f6e145e5fc
spiros <spirostza77@hotmail.com>
2017-05-28 00:28:45 +0300

    hardcoded region for rackspace

--- misttests/api/io/libcloud.py
+++ misttests/api/io/libcloud.py
@@ -70,27 +70,26 @@ class TestLibcloudFunctionality:
         assert len(response.json()) >= 0, "List Vultr machines did not return a proper result"
         print "Success!!!"
 
-    def test_list_machines_azure_arm(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.add_cloud(title='Azure_arm', provider= 'azure_arm', api_token=owner_api_token,
-                                       tenant_id=safe_get_var('clouds/azure_arm', 'tenant_id',
-                                                              config.CREDENTIALS['AZURE_ARM']['tenant_id']),
-                                       subscription_id=safe_get_var('clouds/azure_arm', 'subscription_id',
-                                                              config.CREDENTIALS['AZURE_ARM']['subscription_id']),
-                                       client_key=safe_get_var('clouds/azure_arm', 'client_key',
-                                                              config.CREDENTIALS['AZURE_ARM']['client_key']),
-                                       client_secret=safe_get_var('clouds/azure_arm', 'client_secret',
-                                                              config.CREDENTIALS['AZURE_ARM']['client_secret'])).post()
-        assert_response_ok(response)
-        cache.set('azure_arm_cloud_id', response.json()['id'])
-        response = mist_core.list_machines(cloud_id=cache.get('azure_arm_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) >= 0, "List Azure_arm machines did not return a proper result"
-        print "Success!!!"
+    # def test_list_machines_azure_arm(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.add_cloud(title='Azure_arm', provider= 'azure_arm', api_token=owner_api_token,
+    #                                    tenant_id=safe_get_var('clouds/azure_arm', 'tenant_id',
+    #                                                           config.CREDENTIALS['AZURE_ARM']['tenant_id']),
+    #                                    subscription_id=safe_get_var('clouds/azure_arm', 'subscription_id',
+    #                                                           config.CREDENTIALS['AZURE_ARM']['subscription_id']),
+    #                                    client_key=safe_get_var('clouds/azure_arm', 'client_key',
+    #                                                           config.CREDENTIALS['AZURE_ARM']['client_key']),
+    #                                    client_secret=safe_get_var('clouds/azure_arm', 'client_secret',
+    #                                                           config.CREDENTIALS['AZURE_ARM']['client_secret'])).post()
+    #     assert_response_ok(response)
+    #     cache.set('azure_arm_cloud_id', response.json()['id'])
+    #     response = mist_core.list_machines(cloud_id=cache.get('azure_arm_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) >= 0, "List Azure_arm machines did not return a proper result"
+    #     print "Success!!!"
 
     def test_list_machines_rackspace(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.add_cloud(title='Rackspace', provider= 'rackspace', api_token=owner_api_token,
-                                       region=safe_get_var('clouds/rackspace', 'region',
-                                                           config.CREDENTIALS['RACKSPACE']['region']),
+                                       region='dfw',
                                        username = safe_get_var('clouds/rackspace', 'username',
                                                            config.CREDENTIALS['RACKSPACE']['username']),
                                        api_key = safe_get_var('clouds/rackspace', 'api_key',
@@ -202,11 +201,11 @@ class TestLibcloudFunctionality:
         assert len(response.json()) > 0, "List Vultr sizes did not return any sizes"
         print "Success!!!"
 
-    def test_list_sizes_azure_arm(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.list_sizes(cloud_id=cache.get('azure_arm_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) > 0, "List Azure_arm sizes did not return any sizes"
-        print "Success!!!"
+    # def test_list_sizes_azure_arm(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.list_sizes(cloud_id=cache.get('azure_arm_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) > 0, "List Azure_arm sizes did not return any sizes"
+    #     print "Success!!!"
 
     def test_list_sizes_rackspace(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.list_sizes(cloud_id=cache.get('rackspace_cloud_id', ''), api_token=owner_api_token).get()
@@ -280,11 +279,11 @@ class TestLibcloudFunctionality:
         assert len(response.json()) > 0, "List Vultr images did not return any images"
         print "Success!!!"
 
-    def test_list_images_azure_arm(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.list_images(cloud_id=cache.get('azure_arm_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) > 0, "List Azure_arm images did not return any images"
-        print "Success!!!"
+    # def test_list_images_azure_arm(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.list_images(cloud_id=cache.get('azure_arm_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) > 0, "List Azure_arm images did not return any images"
+    #     print "Success!!!"
 
     def test_list_images_rackspace(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.list_images(cloud_id=cache.get('rackspace_cloud_id', ''), api_token=owner_api_token).get()
