5a87e4bed67dc5cdc24c43361ae95ecbbf950762
spiros <spirostza77@hotmail.com>
2017-05-28 00:53:31 +0300

    dump gce private key

--- misttests/api/io/libcloud.py
+++ misttests/api/io/libcloud.py
@@ -2,6 +2,7 @@ from misttests.api.helpers import *
 from misttests import config
 from misttests.config import safe_get_var
 import pytest
+import json
 
 ############################################################################
 #                         Functional Testing                               #
@@ -11,69 +12,69 @@ import pytest
 @pytest.mark.incremental
 class TestLibcloudFunctionality:
 
-    def test_list_machines_docker(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.add_cloud(title='Docker', provider= 'docker', api_token=owner_api_token,
-                                       docker_host=safe_get_var('clouds/docker', 'host', config.CREDENTIALS['DOCKER']['host']),
-                                       docker_port=safe_get_var('clouds/docker_orchestrator', 'port', config.CREDENTIALS['DOCKER']['port']),
-                                       authentication=safe_get_var('clouds/docker', 'authentication', config.CREDENTIALS['DOCKER']['authentication']),
-                                       ca_cert_file=safe_get_var('clouds/docker', 'ca', config.CREDENTIALS['DOCKER']['ca']),
-                                       key_file=safe_get_var('clouds/docker', 'key', config.CREDENTIALS['DOCKER']['key']),
-                                       cert_file=safe_get_var('clouds/docker', 'cert', config.CREDENTIALS['DOCKER']['cert'])).post()
-        assert_response_ok(response)
-        cache.set('docker_cloud_id', response.json()['id'])
-        response = mist_core.list_machines(cloud_id=cache.get('docker_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) > 0, "List Docker machines did not return any machines"
-        print "Success!!!"
+    # def test_list_machines_docker(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.add_cloud(title='Docker', provider= 'docker', api_token=owner_api_token,
+    #                                    docker_host=safe_get_var('clouds/docker', 'host', config.CREDENTIALS['DOCKER']['host']),
+    #                                    docker_port=safe_get_var('clouds/docker_orchestrator', 'port', config.CREDENTIALS['DOCKER']['port']),
+    #                                    authentication=safe_get_var('clouds/docker', 'authentication', config.CREDENTIALS['DOCKER']['authentication']),
+    #                                    ca_cert_file=safe_get_var('clouds/docker', 'ca', config.CREDENTIALS['DOCKER']['ca']),
+    #                                    key_file=safe_get_var('clouds/docker', 'key', config.CREDENTIALS['DOCKER']['key']),
+    #                                    cert_file=safe_get_var('clouds/docker', 'cert', config.CREDENTIALS['DOCKER']['cert'])).post()
+    #     assert_response_ok(response)
+    #     cache.set('docker_cloud_id', response.json()['id'])
+    #     response = mist_core.list_machines(cloud_id=cache.get('docker_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) > 0, "List Docker machines did not return any machines"
+    #     print "Success!!!"
+    #
+    # def test_list_machines_rackspace(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.add_cloud(title='Rackspace', provider= 'rackspace', api_token=owner_api_token,
+    #                                    region='dfw',
+    #                                    username = safe_get_var('clouds/rackspace', 'username',
+    #                                                        config.CREDENTIALS['RACKSPACE']['username']),
+    #                                    api_key = safe_get_var('clouds/rackspace', 'api_key',
+    #                                                        config.CREDENTIALS['RACKSPACE']['api_key'])).post()
+    #     assert_response_ok(response)
+    #     cache.set('rackspace_cloud_id', response.json()['id'])
+    #     response = mist_core.list_machines(cloud_id=cache.get('rackspace_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) >= 0, "List Rackspace machines did not return a proper result"
+    #     print "Success!!!"
+    #
+    # def test_list_machines_aws(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.add_cloud(title='AWS', provider= 'ec2', api_token=owner_api_token,
+    #                                    api_key=safe_get_var('clouds/aws', 'api_key', config.CREDENTIALS['EC2']['api_key']),
+    #                                    api_secret=safe_get_var('clouds/aws', 'api_secret', config.CREDENTIALS['EC2']['api_secret']),
+    #                                    region='ec2_ap_northeast').post()
+    #     assert_response_ok(response)
+    #     cache.set('aws_cloud_id', response.json()['id'])
+    #     response = mist_core.list_machines(cloud_id=cache.get('aws_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) >= 0, "List AWS machines did not return a proper result"
+    #     print "Success!!!"
+    #
+    # def test_list_machines_digitalocean(self, pretty_print, mist_core, cache, owner_api_token):
+    #     response = mist_core.add_cloud(title='Digital Ocean', provider= 'digitalocean', api_token=owner_api_token,
+    #                                    token=safe_get_var('clouds/digitalocean', 'token', config.CREDENTIALS['DIGITALOCEAN']['token'])).post()
+    #     assert_response_ok(response)
+    #     cache.set('digitalocean_cloud_id', response.json()['id'])
+    #     response = mist_core.list_machines(cloud_id=cache.get('digitalocean_cloud_id', ''), api_token=owner_api_token).get()
+    #     assert_response_ok(response)
+    #     assert len(response.json()) >= 0, "List Digital Ocean machines did not return a proper result"
+    #     print "Success!!!"
 
-    def test_list_machines_rackspace(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.add_cloud(title='Rackspace', provider= 'rackspace', api_token=owner_api_token,
-                                       region='dfw',
-                                       username = safe_get_var('clouds/rackspace', 'username',
-                                                           config.CREDENTIALS['RACKSPACE']['username']),
-                                       api_key = safe_get_var('clouds/rackspace', 'api_key',
-                                                           config.CREDENTIALS['RACKSPACE']['api_key'])).post()
-        assert_response_ok(response)
-        cache.set('rackspace_cloud_id', response.json()['id'])
-        response = mist_core.list_machines(cloud_id=cache.get('rackspace_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) >= 0, "List Rackspace machines did not return a proper result"
-        print "Success!!!"
-
-    def test_list_machines_aws(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.add_cloud(title='AWS', provider= 'ec2', api_token=owner_api_token,
-                                       api_key=safe_get_var('clouds/aws', 'api_key', config.CREDENTIALS['EC2']['api_key']),
-                                       api_secret=safe_get_var('clouds/aws', 'api_secret', config.CREDENTIALS['EC2']['api_secret']),
-                                       region='ec2_ap_northeast').post()
-        assert_response_ok(response)
-        cache.set('aws_cloud_id', response.json()['id'])
-        response = mist_core.list_machines(cloud_id=cache.get('aws_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) >= 0, "List AWS machines did not return a proper result"
-        print "Success!!!"
-
-    def test_list_machines_digitalocean(self, pretty_print, mist_core, cache, owner_api_token):
-        response = mist_core.add_cloud(title='Digital Ocean', provider= 'digitalocean', api_token=owner_api_token,
-                                       token=safe_get_var('clouds/digitalocean', 'token', config.CREDENTIALS['DIGITALOCEAN']['token'])).post()
-        assert_response_ok(response)
-        cache.set('digitalocean_cloud_id', response.json()['id'])
-        response = mist_core.list_machines(cloud_id=cache.get('digitalocean_cloud_id', ''), api_token=owner_api_token).get()
-        assert_response_ok(response)
-        assert len(response.json()) >= 0, "List Digital Ocean machines did not return a proper result"
-        print "Success!!!"
-
-
-#    def test_list_machines_gce(self, pretty_print, mist_core, cache, owner_api_token):
-#        response = mist_core.add_cloud(title='GCE', provider= 'gce', api_token=owner_api_token,
-#                                       email=config.CREDENTIALS['GCE']['private_key']['client_email'],
-#                                       project_id=config.CREDENTIALS['GCE']['project_id'],
-#                                       private_key=config.CREDENTIALS['GCE']['private_key']).post()
-#        assert_response_ok(response)
-#        cache.set('gce_cloud_id', response.json()['id'])
-#        response = mist_core.list_machines(cloud_id=cache.get('gce_cloud_id', ''), api_token=owner_api_token).get()
-#        assert_response_ok(response)
-#        assert len(response.json()) >= 0, "List GCE machines did not return a proper result"
-#        print "Success!!!"
+    def test_list_machines_gce(self, pretty_print, mist_core, cache, owner_api_token):
+       response = mist_core.add_cloud(title='GCE', provider= 'gce', api_token=owner_api_token,
+                                      project_id=safe_get_var('clouds/gce/mist-dev', 'project_id',
+                                                              config.CREDENTIALS['GCE']['project_id']),
+                                      private_key = json.dumps(safe_get_var('clouds/gce/mist-dev', 'private_key',
+                                                              config.CREDENTIALS['GCE']['private_key']))).post()
+       assert_response_ok(response)
+       cache.set('gce_cloud_id', response.json()['id'])
+       response = mist_core.list_machines(cloud_id=cache.get('gce_cloud_id', ''), api_token=owner_api_token).get()
+       assert_response_ok(response)
+       assert len(response.json()) >= 0, "List GCE machines did not return a proper result"
+       print "Success!!!"
 
     def test_list_machines_softlayer(self, pretty_print, mist_core, cache, owner_api_token):
         response = mist_core.add_cloud(title='Softlayer', provider= 'softlayer', api_token=owner_api_token,
