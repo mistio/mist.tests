213eb325cc13c17c7c4c90e1b0955fd53869078c
spiros <spirostza77@hotmail.com>
2017-05-30 00:39:48 +0300

    verify that rule is mentioned in the incident

--- misttests/gui/core/mayday/features/mayday.feature
+++ misttests/gui/core/mayday/features/mayday.feature
@@ -5,8 +5,8 @@ Feature: Production
 #  Scenario: Production monitor and graph testing
 #    Given I am logged in to mist.core
 #    When I visit the Machines page after the counter has loaded
-#    Then I search for the mayday machine
-#    When I click the mayday machine
+    Then I search for the mayday machine
+    When I click the mayday machine
 #    And I clear the machines search bar
 #    And I expect the "machine" edit form to be visible within max 5 seconds
 #    Then I wait for the graphs to appear
--- misttests/gui/steps/alert.py
+++ misttests/gui/steps/alert.py
@@ -5,8 +5,8 @@ from .utils import safe_get_element_text
 @step(u'I should see the incident "{incident}"')
 def check_for_incident(context, incident):
     import ipdb;ipdb.set_trace()
-    app_incidents = context.browser.find_element_by_tag_name('app-incidents')
-    paper_material = app_incidents.find_element_by_tag_name('paper-material')
+    # app_incidents = context.browser.find_element_by_tag_name('app-incidents')
+    # paper_material = app_incidents.find_element_by_tag_name('paper-material')
     incidents_list = context.browser.find_elements_by_css_selector('div.block div.list')
 
     for item in incidents_list:
