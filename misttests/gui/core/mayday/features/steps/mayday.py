55b762a5776801061ffdd73461c4aaf152e19cdc
spiros <spirostza77@hotmail.com>
2017-05-30 00:42:03 +0300

    remove ipdb and comments

--- misttests/gui/core/mayday/features/steps/mayday.py
+++ misttests/gui/core/mayday/features/steps/mayday.py
@@ -113,9 +113,6 @@ def rule_value(context, value):
 
 @step(u'I should see the incident "{incident}"')
 def check_for_incident(context, incident):
-    import ipdb;ipdb.set_trace()
-    # app_incidents = context.browser.find_element_by_tag_name('app-incidents')
-    # paper_material = app_incidents.find_element_by_tag_name('paper-material')
     incidents_list = context.browser.find_elements_by_css_selector('div.block div.list')
 
     for item in incidents_list:
