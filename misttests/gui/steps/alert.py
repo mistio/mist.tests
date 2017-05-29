6f8a8c6102a5ec5871eb9a2a4325ebadf4bb3069
spiros <spirostza77@hotmail.com>
2017-05-29 16:09:55 +0300

    insert step that checks for listed incidents

--- misttests/gui/steps/alert.py
+++ misttests/gui/steps/alert.py
@@ -5,5 +5,6 @@ from behave import step
 def check_for_incident(context, incident):
     import ipdb;ipdb.set_trace()
     incidents = context.browser.find_element_by_tag_name('app-incidents')
-
+    incidents_list = incidents.find_element_by_tag_name('paper-material')
+    listed_incidents = incidents_list.find_elements_by_class_name('list-item')
     return
