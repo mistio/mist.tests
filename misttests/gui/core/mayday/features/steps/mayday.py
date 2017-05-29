788500062380929f0327a799a9659cd7fb71f169
spiros <spirostza77@hotmail.com>
2017-05-30 00:41:37 +0300

    remove alert.py

--- misttests/gui/core/mayday/features/steps/mayday.py
+++ misttests/gui/core/mayday/features/steps/mayday.py
@@ -109,3 +109,16 @@ def rule_value(context, value):
 #    value_input.send_keys(u'\ue003')
 #    context.execute_steps(u'When I wait for 2 seconds')
 #    value_input.send_keys(value)
+
+
+@step(u'I should see the incident "{incident}"')
+def check_for_incident(context, incident):
+    import ipdb;ipdb.set_trace()
+    # app_incidents = context.browser.find_element_by_tag_name('app-incidents')
+    # paper_material = app_incidents.find_element_by_tag_name('paper-material')
+    incidents_list = context.browser.find_elements_by_css_selector('div.block div.list')
+
+    for item in incidents_list:
+        if incident in safe_get_element_text(item):
+            return
+    assert False, "Incident %s was not found in the home page" % incident
