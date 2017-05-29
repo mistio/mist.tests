6fa9fac926b9d2f4eb46e41028fe293a725f6ce7
spiros <spirostza77@hotmail.com>
2017-05-30 00:37:37 +0300

    successfully obtaining incidents list

--- misttests/gui/steps/alert.py
+++ misttests/gui/steps/alert.py
@@ -1,18 +1,15 @@
 from behave import step
-
-
+from .utils import safe_get_element_text
 
 
 @step(u'I should see the incident "{incident}"')
 def check_for_incident(context, incident):
-    from .buttons import clicketi_click
-
     import ipdb;ipdb.set_trace()
     app_incidents = context.browser.find_element_by_tag_name('app-incidents')
-    incidents_list = app_incidents.find_element_by_tag_name('paper-material')
-    test = context.browser.find_element_by_css_selector('div.block div.list')
-    elem = context.browser.find_elements_by_xpath("//paper-material[contains(@id,'d')]/div")
-    # div = incidents_list.find_elements_by_class_name('app-incidents')
-    # inner_div = div.find_element_by_tag_name('div')
-    # listed_incidents = inner_div.find_elements_by_tag_name('a')
-    return
+    paper_material = app_incidents.find_element_by_tag_name('paper-material')
+    incidents_list = context.browser.find_elements_by_css_selector('div.block div.list')
+
+    for item in incidents_list:
+        if incident in safe_get_element_text(item):
+            return
+    assert False, "Incident %s was not found in the home page" % incident
