571654930d4f48e2520116ca7ef9975b8ca52b51
spiros <spirostza77@hotmail.com>
2017-06-02 19:04:33 +0300

    insert headless in chromedriver args

--- misttests/config.py
+++ misttests/config.py
@@ -207,9 +207,7 @@ ORG_ID = get_setting('ORG_ID', '')
 
 SETUP_ENVIRONMENT = get_setting("SETUP_ENVIRONMENT", False)
 
-WEBDRIVER_OPTIONS = get_setting('WEBDRIVER_OPTIONS',
-                                    ['--headless', 'window-size=1200x600'])
-                                 # ['--dns-prefetch-disable'])
+WEBDRIVER_OPTIONS = get_setting('WEBDRIVER_OPTIONS', ['--headless', '--window-size=1200x600'])
 
 REGISTER_USER_BEFORE_FEATURE = get_setting('REGISTER_USER_BEFORE_FEATURE', True, priority='environment')
 
