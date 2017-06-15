780bf58cd1af1f64ca54ca20fdd6d243220ea580
spiros <spirostza77@hotmail.com>
2017-06-15 16:36:09 +0300

    insert step that refreshes page until insights data are available

--- misttests/gui/steps/insights.py
+++ misttests/gui/steps/insights.py
@@ -2,6 +2,7 @@ from behave import step
 from time import time
 
 from selenium.common.exceptions import NoSuchElementException
+from .utils import safe_get_element_text
 
 
 @step(u'the "{section}" section should be visible within {seconds} seconds')
@@ -16,6 +17,15 @@ def check_insights_element_visibility(context, section, seconds):
     assert False, "Section %s is not visible after %s seconds" % (section,seconds)
 
 
+@step(u'"{element}" in "{section}" section should be "{value}"')
+def check_value_in_section(context, element, section, value):
+    section_element = context.browser.find_element_by_id(section)
+    data_element = section_element.find_element_by_id(section + '-data')
+    element_to_check = data_element.find_element_by_id(section + '-' + element)
+    import ipdb;ipdb.set_trace()
+    assert value in safe_get_element_text(element_to_check)
 
-@step(u'"{element}" in "{section}" section should be "value"')
-def check value_in_section(context, )
+
+@step(u'I refresh the Insights page until data are available')
+def refresh_until_data_are_available(context):
+    return 
