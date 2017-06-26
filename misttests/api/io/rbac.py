7ae134d8f3304e8cdcf5853467e81c8b8c284b2c
spiros <spirostza77@hotmail.com>
2017-06-26 22:19:18 +0300

    list orgs ok

--- misttests/api/io/rbac.py
+++ misttests/api/io/rbac.py
@@ -70,11 +70,38 @@ class TestRbacFunctionality:
         name = 'test_org_%d' % random.randint(1, 2000)
         response = mist_core.create_org(api_token=owner_api_token, name=name).post()
         cache.set('org_id', response.json()['id'])
+        import ipdb;ipdb.set_trace()
         assert_response_ok(response)
         response = mist_core.create_org(api_token=owner_api_token, name=name).post()
         assert_response_conflict(response)
         print "Success!!!"
 
-    def test_add_team(self, pretty_print, mist_core, owner_api_token, cache):
-        response = mist_core.add_team(api_token=owner_api_token,
-                                      name='test_team', org_id=cache.get('org_id', '')).post()
+    def test_list_orgs(self, pretty_print, mist_core, owner_api_token, cache):
+        response = mist_core.list_orgs(api_token=owner_api_token).get()
+        import ipdb;
+        ipdb.set_trace()
+
+    # def test_show_user_org(self, pretty_print, mist_core, owner_api_token):
+    #     response = mist_core.show_user_org(api_token=owner_api_token).get()
+    #     import ipdb;ipdb.set_trace()
+
+
+    # def test_add_team(self, pretty_print, mist_core, owner_api_token, cache):
+    #     response = mist_core.add_team(api_token=owner_api_token,
+    #                                   name='test_team', org_id=cache.get('org_id', '')).post()
+    #     import ipdb;ipdb.set_trace()
+
+
+# show_org
+# show_user_org
+
+# show_user_invitations
+
+# add team
+# list teams
+# show team
+# edit team
+# delete team
+
+# invite member
+# delete member
