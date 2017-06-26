a0979edfd83d74875b63ea182ba9c8c24cc8c084
spiros <spirostza77@hotmail.com>
2017-06-27 00:53:05 +0300

    verify len of teams and members in organization in show_org

--- misttests/api/io/rbac.py
+++ misttests/api/io/rbac.py
@@ -76,15 +76,6 @@ def test_show_user_org_no_api_token(pretty_print, mist_core):
 @pytest.mark.incremental
 class TestRbacFunctionality:
 
-    def test_create_org(self, pretty_print, mist_core, owner_api_token, cache):
-        name = 'test_org_%d' % random.randint(1, 2000)
-        response = mist_core.create_org(api_token=owner_api_token, name=name).post()
-        cache.set('org_id', response.json()['id'])
-        assert_response_ok(response)
-        response = mist_core.create_org(api_token=owner_api_token, name=name).post()
-        assert_response_conflict(response)
-        print "Success!!!"
-
     def test_list_orgs(self, pretty_print, mist_core, owner_api_token, cache):
         response = mist_core.list_orgs(api_token=owner_api_token).get()
         assert len(response.json()) == 2, "User should belong to 2 orgs, but instead belongs to %s" % len(response.json())
@@ -94,7 +85,18 @@ class TestRbacFunctionality:
     def test_show_user_org(self, pretty_print, mist_core, owner_api_token):
         response = mist_core.show_user_org(api_token=owner_api_token).get()
         import ipdb;ipdb.set_trace()
+        assert len(response.json()['members_count']) == 1, "The brand new org has more than 1 members!!!"
+        assert len(response.json()['teams']) == 1, "The brand new org has more than 1 teams!!!"
+        print "Success!!!"
 
+    def test_create_org(self, pretty_print, mist_core, owner_api_token, cache):
+        name = 'test_org_%d' % random.randint(1, 2000)
+        response = mist_core.create_org(api_token=owner_api_token, name=name).post()
+        cache.set('org_id', response.json()['id'])
+        assert_response_ok(response)
+        response = mist_core.create_org(api_token=owner_api_token, name=name).post()
+        assert_response_conflict(response)
+        print "Success!!!"
 
     # def test_add_team(self, pretty_print, mist_core, owner_api_token, cache):
     #     response = mist_core.add_team(api_token=owner_api_token,
@@ -105,8 +107,9 @@ class TestRbacFunctionality:
 
 
 
-# show_user_org
 # show_user_invitations
+
+# switch between orgs?
 
 # add team
 # list teams
