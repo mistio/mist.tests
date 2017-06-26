8e540b8e837e35e95c569b54b62cde5fa4c522bb
spiros <spirostza77@hotmail.com>
2017-06-26 20:41:43 +0300

    create org tests completed

--- misttests/api/core/rbac.py
+++ misttests/api/core/rbac.py
@@ -12,3 +12,15 @@ def test_create_org_missing_parameter(pretty_print, mist_core, owner_api_token):
     response = mist_core.create_org(api_token=owner_api_token).post()
     assert_response_bad_request(response)
     print "Success!!!"
+
+
+def test_create_org_no_api_token(pretty_print, mist_core):
+    response = mist_core.create_org(api_token='', name='test_org').post()
+    assert_response_forbidden(response)
+    print "Success!!!"
+
+
+def test_create_org_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.create_org(api_token='00' + owner_api_token[:-2], name='test_org').post()
+    assert_response_unauthorized(response)
+    print "Success!!!"
