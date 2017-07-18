1c63cf86fc9af2833a8bab70de768d4224f7b5aa
spiros <spirostza77@hotmail.com>
2017-07-18 15:46:38 +0300

    insert zones UI tests in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -98,7 +98,7 @@ vault_login() {
     behave_tags["monitoring"]='monitoring-locally'
     behave_tags["orchestration"]='orchestration,'
     behave_tags["rbac-rules"]='rbac-rules-1'
-
+    behave_tags["zones"]='zones'
 
 
     if [ "$#" -eq 0 ]
