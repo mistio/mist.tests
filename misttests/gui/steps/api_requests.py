fec5bdc6f7ae13f677685e278fa1469a98a0df9d
spiros <spirostza77@hotmail.com>
2017-07-18 18:25:29 +0300

    pep8 fix

--- misttests/gui/steps/api_requests.py
+++ misttests/gui/steps/api_requests.py
@@ -145,7 +145,7 @@ def add_docker_api_request(context, cloud):
             'dns_enabled': True
         }
 
-    re = requests.post(context.mist_config['MIST_URL'] + "/api/v1/clouds", data=json.dumps(payload), headers=headers)
+    requests.post(context.mist_config['MIST_URL'] + "/api/v1/clouds", data=json.dumps(payload), headers=headers)
 
 
 @step(u'Docker machine "{machine_name}" has been added via API request')
