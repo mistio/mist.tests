cf1dc467b3c5be70ac7aab4184be1ced88cc45ed
spiros <spirostza77@hotmail.com>
2017-07-20 18:28:03 +0300

    invoke run_tests.sh using source so that vault client token is stored from the 1st time

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
 XVFB_WHD=${XVFB_WHD:-1280x1024x16}
@@ -15,4 +16,4 @@ cd /mist.core/src/mist.io/tests
 
 sleep .5
 
-./run_tests.sh && /bin/bash
+source run_tests.sh && /bin/bash
