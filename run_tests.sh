efea14cef3ec9fda4e90849a71fbe3c988ba005f
spiros <spirostza77@hotmail.com>
2017-07-22 01:27:22 +0300

    minor fixes

--- run_tests.sh
+++ run_tests.sh
@@ -33,7 +33,7 @@ run_gui_tests_suite() {
     #   behave_tags+="${tag}"
     # done
 
-    behave -k --no-capture --no-capture-stderr --tags=clouds-actions,images-networks,rbac-rules-1,orchestration,scripts-actions,user-actions,machines,monitoring-locally,keys,scripts,zones,rbac-teams misttests/gui/core/pr/features
+    behave -k --no-capture --no-capture-stderr --tags=clouds-actions,images-networks,orchestration,user-actions,machines,monitoring-locally,keys,scripts,zones,rbac-teams misttests/gui/core/pr/features
 }
 
 run_api_tests_suite() {
