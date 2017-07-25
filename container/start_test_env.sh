f07775c951132920de75c0432b21569194368cac
spiros <spirostza77@hotmail.com>
2017-07-25 16:46:23 +0300

    remove comment

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -7,7 +7,6 @@ XVFB_WHD=${XVFB_WHD:-1280x1024x16}
 export DISPLAY=:1.0
 export MIST_URL=${MIST_URL:-http://172.17.0.1}
 
-# Start Xvfb
 Xvfb :1 -ac -screen 0 $XVFB_WHD &
 
 x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
