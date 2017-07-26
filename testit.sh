a5c539cc9b6dd51a9aeb084c81491162bb7567e4
spiros <spirostza77@hotmail.com>
2017-07-26 20:40:21 +0300

    properly running custom api or gui tests

--- testit.sh
+++ testit.sh
@@ -25,6 +25,29 @@ help_message() {
     echo
 }
 
+vault_login() {
+    if [ -z "$VAULT_CLIENT_TOKEN" ]
+    then
+        export vault_server=${VAULT_ADDR:-https://vault.ops.mist.io:8200}
+        echo Vault username:
+        read username
+        echo Vault password:
+        read -s password
+        export PYTHONIOENCODING=utf8
+        VAULT_CLIENT_TOKEN=$(curl $vault_server/v1/auth/userpass/login/$username -d '{ "password": "'${password}'" }' |
+         python -c "import sys, json; print(json.load(sys.stdin)['auth']['client_token'])")
+
+        if [[ -z "${VAULT_CLIENT_TOKEN// }" ]]
+        then
+            echo 'Wrong credentials given...'
+            vault_login
+        else
+            export VAULT_CLIENT_TOKEN
+            echo 'Successfully logged in. About to start running tests...'
+        fi
+    fi
+}
+
 declare -A pytest_paths
 
 pytest_paths["clouds"]='misttests/api/io/clouds.py'
@@ -41,13 +64,39 @@ pytest_paths["tunnels"]='misttests/api/core/tunnels.py'
 pytest_paths["orchestration"]='misttests/api/core/orchestration.py'
 pytest_paths["rbac"]='misttests/api/io/rbac.py'
 
+declare -A behave_tags
+
+behave_tags["clouds"]='clouds-add-1','clouds-add-2'
+behave_tags["clouds-actions"]='clouds-actions,'
+behave_tags["images"]='images-networks,'
+behave_tags["keys"]='keys,'
+behave_tags["scripts"]='scripts','scripts-actions'
+behave_tags["machines"]='machines,'
+behave_tags["users"]='user-actions,'
+behave_tags["rbac"]='rbac-teams'
+behave_tags["schedules"]='schedulers-1','schedulers-2,'
+behave_tags["monitoring"]='monitoring-locally'
+behave_tags["orchestration"]='orchestration,'
+behave_tags["rbac-rules"]='rbac-rules-1',
+behave_tags["zones"]='zones,'
+
 run_api_tests_suite() {
+  echo "Running entire api tests suite..."
   pytest_args=""
   for path in "${pytest_paths[@]}"
   do
     pytest_args="${pytest_args} ${path}"
   done
   pytest $pytest_args
+}
+
+run_gui_tests_suite() {
+    behave_tags=""
+    for tag in "${behave_tags[@]}"
+    do
+      behave_tags+="${tag}"
+    done
+    behave -k --no-capture --no-capture-stderr --tags=$behave_tags misttests/gui/core/pr/features
 }
 
 validate_api_args(){
@@ -61,7 +110,18 @@ validate_api_args(){
   done
 }
 
-while getopts ":a:" opt; do
+validate_gui_args(){
+  for arg in $@
+  do
+    if [ -z "${behave_tags["$arg"]}" ]
+    then
+      help_message
+      exit
+    fi
+  done
+}
+
+while getopts ":a:g:" opt; do
   case $opt in
     a)
       IFS=','
@@ -77,14 +137,25 @@ while getopts ":a:" opt; do
         done
       fi
       ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
+    g)
+      IFS=','
+      echo "Gui tests will be triggered. Parameters are: $OPTARG"
+      if [ -z "$OPTARG" ]
+      then
+        run_gui_tests_suite
+      else
+        validate_gui_args "$OPTARG"
+        for arg in $OPTARG
+        do
+            behave -k --no-capture --no-capture-stderr --stop --tags=${behave_tags["$arg"]} misttests/gui/core/pr/features
+        done
+      fi
       ;;
-    :)
-      run_api_tests_suite
-      #echo "Option -$OPTARG requires an argument." >&2
-      #exit 1
+   \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+   :)
+      echo nothing
       ;;
   esac
 done
