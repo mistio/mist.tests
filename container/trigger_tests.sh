e96d143d907b21dd9479ce7e99fee51a2c80aa3f
spiros <spirostza77@hotmail.com>
2017-08-01 16:22:11 +0300

    add another script for entrypoint when running tests

---
+++ container/trigger_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+XVFB_WHD=${XVFB_WHD:-1280x1024x16}
+
+export DISPLAY=:1.0
+export MIST_URL=${MIST_URL:-http://172.17.0.1}
+
+Xvfb :1 -ac -screen 0 $XVFB_WHD &
+
+x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
+
+cd /mist.core/src/mist.io/tests
+
+sleep .5
+
+source testit.sh $@ clouds && /bin/bash
