d99af761a0ab9ad2169e7f6d1917abf39041dc53
spiros <spirostza77@hotmail.com>
2017-08-11 14:43:52 +0300

    whitelist_ips no api token

--- misttests/api/io/ip_whitelisting.py
+++ misttests/api/io/ip_whitelisting.py
@@ -7,3 +7,9 @@ import pytest
 ############################################################################
 #                             Unit Testing                                 #
 ############################################################################
+
+
+def test_whitelist_ips_no_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.whitelist_ips(api_token='').post()
+    assert_response_unauthorized(response)
+    print "Success!!!"
