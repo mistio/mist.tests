fd5c8a61affc8f4e20eb06ead6bb3743d337cef7
spiros <spirostza77@hotmail.com>
2017-08-11 17:55:13 +0300

    ip_whitelisting missing param

--- misttests/api/io/ip_whitelisting.py
+++ misttests/api/io/ip_whitelisting.py
@@ -18,3 +18,12 @@ def test_whitelist_ips_wrong_api_token(pretty_print, mist_core, owner_api_token)
     response = mist_core.whitelist_ips(api_token='00' + owner_api_token[:-2]).post()
     assert_response_unauthorized(response)
     print "Success!!!"
+
+def test_whitelist_ips_missing parameter(pretty_print, mist_core, owner_api_token):
+    response = mist_core.whitelist_ips(api_token=owner_api_token).post()
+    assert_response_bad_request(response)
+    print "Success!!!"
+
+
+#request_whitelist_ip
+#confirm_whitelist_ip
