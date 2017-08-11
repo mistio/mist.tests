ed41b645537aa85c85f8ec541157f87f8ea5259d
spiros <spirostza77@hotmail.com>
2017-08-11 17:32:22 +0300

    whitelist ips wrong api token

--- misttests/api/io/ip_whitelisting.py
+++ misttests/api/io/ip_whitelisting.py
@@ -11,5 +11,10 @@ import pytest
 
 def test_whitelist_ips_no_api_token(pretty_print, mist_core, owner_api_token):
     response = mist_core.whitelist_ips(api_token='').post()
+    assert_response_forbidden(response)
+    print "Success!!!"
+
+def test_whitelist_ips_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.whitelist_ips(api_token='00' + owner_api_token[:-2]).post()
     assert_response_unauthorized(response)
     print "Success!!!"
