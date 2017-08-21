b1474dc320aad09fa742468dcb6f1001682a9909
spiros <spirostza77@hotmail.com>
2017-08-21 13:53:34 +0300

    reinsert api tests

--- misttests/api/io/ip_whitelisting.py
+++ misttests/api/io/ip_whitelisting.py
@@ -4,28 +4,25 @@ from misttests import config
 import pytest
 import socket
 
-from time import sleep
-
-
 ############################################################################
 #                             Unit Testing                                 #
 ############################################################################
 
 
-#def test_whitelist_ips_no_api_token(pretty_print, mist_core):
-#    response = mist_core.whitelist_ips(api_token='').post()
-#    assert_response_forbidden(response)
-#    print "Success!!!"
+def test_whitelist_ips_no_api_token(pretty_print, mist_core):
+    response = mist_core.whitelist_ips(api_token='').post()
+    assert_response_forbidden(response)
+    print "Success!!!"
 
-#def test_whitelist_ips_wrong_api_token(pretty_print, mist_core, owner_api_token):
-#    response = mist_core.whitelist_ips(api_token='00' + owner_api_token[:-2]).post()
-#    assert_response_unauthorized(response)
-#    print "Success!!!"
+def test_whitelist_ips_wrong_api_token(pretty_print, mist_core, owner_api_token):
+    response = mist_core.whitelist_ips(api_token='00' + owner_api_token[:-2]).post()
+    assert_response_unauthorized(response)
+    print "Success!!!"
 
-#def test_whitelist_ips_missing_parameter(pretty_print, mist_core, owner_api_token):
-#    response = mist_core.whitelist_ips(api_token=owner_api_token).post()
-#    assert_response_bad_request(response)
-#    print "Success!!!"
+def test_whitelist_ips_missing_parameter(pretty_print, mist_core, owner_api_token):
+    response = mist_core.whitelist_ips(api_token=owner_api_token).post()
+    assert_response_bad_request(response)
+    print "Success!!!"
 
 def test_request_whitelist_ip_no_api_token(pretty_print, mist_core):
     response = mist_core.request_whitelist_ip(api_token='').post()
