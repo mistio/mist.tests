ae74fa744357cdd3175868363042e40cc18bf799
spiros <spirostza77@hotmail.com>
2017-08-21 18:17:00 +0300

    insert step that whitelists an IP

--- misttests/gui/core/pr/features/steps/ip_whitelisting.py
+++ misttests/gui/core/pr/features/steps/ip_whitelisting.py
@@ -1,6 +1,8 @@
 from behave import step
 
 from misttests.gui.steps.buttons import clicketi_click
+from misttests.gui.steps.forms import clear_input_and_send_keys
+
 
 
 @step(u'I remove all whitelisted ips')
@@ -15,3 +17,12 @@ def remove_whitelisted_ips(context):
                     And I click the "Remove Cidr" button in the dialog "Remove Cidr"
                     And I expect the dialog "Remove Cidr" is closed within 4 seconds
                 ''')
+
+@step(u'I add the IP "{ip}" as whitelisted')
+def add_whitelisted_ip(context,ip):
+    import ipdb;ipdb.set_trace()
+    whitelisted_ips_tag = context.browser.find_element_by_tag_name('multi-inputs')
+    new_whitelisted_ip_field = whitelisted_ips_tag.find_element_by_tag_name('div')
+    whitelisted_ip = new_whitelisted_ip_field.find_element_by_tag_name('paper-input')
+    input = whitelisted_ip.find_element_by_id('input')
+    clear_input_and_send_keys(input, ip)
