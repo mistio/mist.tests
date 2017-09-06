3b80668dda6b327588bb7c203b4c5e63ad4d0bb0
spiros <spirostza77@hotmail.com>
2017-09-06 18:02:55 +0300

    reenable no-sandbox flag

--- misttests/helpers/selenium_utils.py
+++ misttests/helpers/selenium_utils.py
@@ -24,7 +24,7 @@ def choose_driver(flavor=None):
         options = webdriver.ChromeOptions()
         options.binary_location = '/usr/bin/google-chrome'
         options.add_argument('headless')
-        #options.add_argument('no-sandbox')
+        options.add_argument('no-sandbox')
         #options.add_argument('disable-gpu')
         options.add_argument('window-size=1920x1080')
         #chrome_options = Options()
