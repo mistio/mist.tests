33a7b74ae33d58028384bfe2f73c663ca5a5825c
spiros <spirostza77@hotmail.com>
2017-09-06 17:15:57 +0300

    no xvfb once again

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -8,7 +8,7 @@ export DISPLAY=:1.0
 #export MIST_URL=${MIST_URL:-http://172.17.0.1}
 
 # Start Xvfb
-Xvfb :1 -ac -screen 0 $XVFB_WHD &
+#Xvfb :1 -ac -screen 0 $XVFB_WHD &
 
 x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
 
