436733769c8cd77ee310d4e27acc24815a0b6afd
spiros <spirostza77@hotmail.com>
2017-09-06 15:41:37 +0300

    reenable xvfb

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-#XVFB_WHD=${XVFB_WHD:-1280x1024x16}
+XVFB_WHD=${XVFB_WHD:-1280x1024x16}
 
-#export DISPLAY=:1.0
+export DISPLAY=:1.0
 #export MIST_URL=${MIST_URL:-http://172.17.0.1}
 
 # Start Xvfb
-#Xvfb :1 -ac -screen 0 $XVFB_WHD &
+Xvfb :1 -ac -screen 0 $XVFB_WHD &
 
-#x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
+x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
 
 # if below commented out, ipdb not working
 cd /mist.core/mist.io/tests
