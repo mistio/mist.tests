e847bfd9e254d48fbc90b300f0da49455a362442
spiros <spirostza77@hotmail.com>
2017-09-12 00:32:15 +0300

    export MIST_URL

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -5,7 +5,7 @@ set -e
 XVFB_WHD=${XVFB_WHD:-1280x1024x16}
 
 export DISPLAY=:1.0
-#export MIST_URL=${MIST_URL:-http://172.17.0.1}
+export MIST_URL=${MIST_URL:-http://172.17.0.1}
 
 #Xvfb :1 -ac -screen 0 $XVFB_WHD &
 
