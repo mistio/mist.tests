bae36af56155af22137e6da69a2451792c9a4aa3
spiros <spirostza77@hotmail.com>
2017-09-14 14:33:45 +0300

    Remove comments

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-XVFB_WHD=${XVFB_WHD:-1280x1024x16}
-
 export DISPLAY=:1.0
 export MIST_URL=${MIST_URL:-http://172.17.0.1}
-
-#Xvfb :1 -ac -screen 0 $XVFB_WHD &
 
 x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
 
 # if below commented out, ipdb not working
 cd /mist.core/mist.io/tests
 
-#sleep .5
+sleep .5
 
 /bin/bash
