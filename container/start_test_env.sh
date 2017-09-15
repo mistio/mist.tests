fa61d80f4bd4022f1cfc7a0779d94368dbd1d7b1
spiros <spirostza77@hotmail.com>
2017-09-15 16:33:55 +0300

    No need for x11vnc

--- container/start_test_env.sh
+++ container/start_test_env.sh
@@ -5,9 +5,6 @@ set -e
 export DISPLAY=:1.0
 export MIST_URL=${MIST_URL:-http://172.17.0.1}
 
-x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
-
-# if below commented out, ipdb not working
 cd /mist.core/mist.io/tests
 
 sleep .5
