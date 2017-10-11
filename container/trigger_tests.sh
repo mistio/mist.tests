7b62ff53b2d86b1ef7b0f4c4661aca1e3bffaa64
spiros <spirostza77@hotmail.com>
2017-10-12 00:47:00 +0300

    Remove xvfb, x11vnc related actions

--- container/trigger_tests.sh
+++ container/trigger_tests.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
-
 set -e
-
-XVFB_WHD=${XVFB_WHD:-1280x1024x16}
-
 export DISPLAY=:1.0
 export MIST_URL=${MIST_URL:-http://172.17.0.1}
+args=$@
 
-Xvfb :1 -ac -screen 0 $XVFB_WHD &
+vault_login() {
+    if [ -z "$VAULT_CLIENT_TOKEN" ]
+    then
+        export vault_server=${VAULT_ADDR:-https://vault.ops.mist.io:8200}
+        echo Vault username:
+        read username
+        echo Vault password:
+        read -s password
+        export PYTHONIOENCODING=utf8
 
-x11vnc -nopw -display :1.0 -listen 0.0.0.0 -rfbport 5900 &
+        VAULT_CLIENT_TOKEN=$(
+          curl -sSk $vault_server/v1/auth/userpass/login/$username -d '{ "password": "'${password}'" }' |
+          python -c "
+import sys, json;
+res = json.load(sys.stdin)
+if res.get('auth'):
+    print(res['auth']['client_token'])"
+        )
+
+        if [[ -z "${VAULT_CLIENT_TOKEN// }" ]]
+        then
+            echo 'Wrong credentials given...'
+            vault_login
+        else
+            export VAULT_CLIENT_TOKEN
+            echo 'Successfully logged in.'
+        fi
+    fi
+}
+
+mkdir /data
+cd /data
+python -m SimpleHTTPServer 8222 2&>1 > /dev/null &
 
 cd /mist.core/mist.io/tests
 
-sleep .5
+vault_login
 
-source run_tests.sh $@ && /bin/bash
+./run_tests.sh $args
+
+/bin/bash
