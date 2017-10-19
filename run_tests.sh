adf5c4df1b80c3a86cc4fea68b7192fbc48021db
spiros <spirostza77@hotmail.com>
2017-10-20 00:02:16 +0300

    All rbac-rules tests are now included

--- run_tests.sh
+++ run_tests.sh
@@ -34,6 +34,7 @@ behave_tags["monitoring"]='monitoring-locally'
 behave_tags["orchestration"]='orchestration'
 behave_tags["rbac-rules-1"]='rbac-rules-1'
 behave_tags["rbac-rules-2"]='rbac-rules-2'
+behave_tags["rbac-rules-3"]='rbac-rules-3'
 behave_tags["rbac-rules-4"]='rbac-rules-4'
 behave_tags["zones"]='zones'
 behave_tags["insights"]='insights'
@@ -59,7 +60,7 @@ help_message() {
     echo
     echo "Argument for UI tests can be one of the following:"
     echo
-    echo "clouds, clouds-actions, machines, images, keys, scripts, users, rbac, schedules, orchestration, monitoring, rbac-rules-1, rbac-rules-2, rbac-rules-4, insights, ip-whitelisting"
+    echo "clouds, clouds-actions, machines, images, keys, scripts, users, rbac, schedules, orchestration, monitoring, rbac-rules-1, rbac-rules-2, rbac-rules-3, rbac-rules-4, insights, ip-whitelisting"
     echo
 }
 
