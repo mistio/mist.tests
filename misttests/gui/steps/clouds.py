6abae142373fffee0d05c5db3b0939ebefe647b2
spiros <spirostza77@hotmail.com>
2017-10-30 18:38:45 +0200

    For now, hardcode host machine

--- misttests/gui/steps/clouds.py
+++ misttests/gui/steps/clouds.py
@@ -186,27 +186,26 @@ def set_azure_arm_creds(context):
 def set_kvm_creds(context):
     context.execute_steps(u'''
                     Then I set the value "KVM" to field "Title" in "cloud" add form
-                    Then I set the value "%s" to field "KVM hostname" in "cloud" add form
+                    Then I set the value "54.199.158.253" to field "KVM hostname" in "cloud" add form
                     And I wait for 1 seconds
                     And I open the "SSH Key" drop down
                     And I wait for 2 seconds
                     And I click the button "KVMKEY" in the "SSH Key" dropdown
                     And I wait for 1 seconds
-                    And I set the value "spiros" to field "SSH USER" in "cloud" add form
-                '''% (safe_get_var('clouds/other_server', 'hostname', context.mist_config['CREDENTIALS']['KVM']['hostname']),))
+                    And I set the value "ubuntu" to field "SSH USER" in "cloud" add form
+                ''' #% (safe_get_var('clouds/other_server', 'hostname', context.mist_config['CREDENTIALS']['KVM']['hostname']),))
 
 
 def set_other_server_creds(context):
     context.execute_steps(u'''
                     Then I set the value "Bare Metal" to field "Title" in "cloud" add form
-                    Then I set the value "%s" to field "Hostname" in "cloud" add form
+                    Then I set the value "54.199.158.253" to field "Hostname" in "cloud" add form
                     And I wait for 1 seconds
                     And I open the "SSH Key" drop down
                     And I wait for 2 seconds
                     And I click the button "KVMKEY" in the "SSH Key" dropdown
                     And I wait for 1 seconds
-                    And I set the value "spiros" to field "User" in "cloud" add form
-                ''' % (safe_get_var('clouds/other_server', 'hostname', context.mist_config['CREDENTIALS']['KVM']['hostname']),))
+                '''# % (safe_get_var('clouds/other_server', 'hostname', context.mist_config['CREDENTIALS']['KVM']['hostname']),))
 
 
 def set_vmware_creds(context):
