802d1c25b20b277d15ac5ff4d70672509b1a3fe4
Dimitris Moraitis <dimo@mist.io>
2018-09-07 13:43:59 +0300

    Remove menu check when waiting for dashboard to load

--- misttests/integration/gui/steps/navigation.py
+++ misttests/integration/gui/steps/navigation.py
@@ -89,7 +89,7 @@ def make_sure_menu_is_open(context):
 
 @step(u'I wait for the links in homepage to appear')
 def wait_for_buttons_to_appear(context):
-    context.execute_steps(u'Then I make sure the menu is open')
+    #context.execute_steps(u'Then I make sure the menu is open')
     end_time = time() + 10
     while time() < end_time:
         try:
