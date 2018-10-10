9df0420dc6fecba8ab6892dd964287a749981454
Dimitris Moraitis <dimo@mist.io>
2018-10-10 19:59:45 +0300

    Wait for the incident to render in mayday test

--- misttests/mayday/features/steps/mayday.py
+++ misttests/mayday/features/steps/mayday.py
@@ -167,10 +167,10 @@ def rule_value(context, value):
 
 @step(u'I should see the incident "{incident}"')
 def check_for_incident(context, incident):
-    incidents_list = context.browser.find_elements_by_css_selector('div.block div.list')
+    incidents_list = context.browser.find_elements_by_css_selector('span.rule-condition.app-incidents')
 
     for item in incidents_list:
-        if incident in safe_get_element_text(item) :
+        if incident in safe_get_element_text(item):
             return
 
     assert False, "Incident %s was not found in the home page" % incident
