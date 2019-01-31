993dc4d728c1663d64ee0ddabf6facd8640c926e
Dimitris Moraitis <dimo@mist.io>
2019-01-31 03:51:39 +0200

    Update search steps

--- misttests/integration/gui/steps/search.py
+++ misttests/integration/gui/steps/search.py
@@ -14,7 +14,10 @@ def search_for_something(context, search_text):
     mist_header_shadow = expand_shadow_root(context, mist_header)
     top_search = mist_header_shadow.find_element_by_css_selector('top-search')
     top_search_shadow = expand_shadow_root(context, top_search)
-    search_field = top_search_shadow.find_element_by_css_selector('input#query')
+    mist_filter = top_search_shadow.find_element_by_css_selector('mist-filter')
+    mist_filter_shadow = expand_shadow_root(context, mist_filter)
+    search_field = mist_filter_shadow.find_element_by_css_selector('paper-input#searchInput')
+    mist_filter_shadow.find_element_by_css_selector('iron-icon').click()
     if context.mist_config.get(search_text):
         search_text = context.mist_config.get(search_text)
     clear_input_and_send_keys(search_field, search_text)
@@ -29,8 +32,11 @@ def clear_search(context):
     mist_header_shadow = expand_shadow_root(context, mist_header)
     top_search = mist_header_shadow.find_element_by_css_selector('top-search')
     top_search_shadow = expand_shadow_root(context, top_search)
-    clear_icons = top_search_shadow.find_elements_by_css_selector(
-        'iron-icon[icon="close"]')
+    mist_filter = top_search_shadow.find_element_by_css_selector('mist-filter')
+    mist_filter_shadow = expand_shadow_root(context, mist_filter)
+    mist_filter_shadow.find_element_by_css_selector('iron-icon').click()
+    clear_icons = mist_filter_shadow.find_elements_by_css_selector(
+        'iron-icon[paper-icon-button="close"]')
     clear_icons = filter(lambda el: el.is_displayed(), clear_icons)
     assert len(clear_icons) > 0, "No clear icon found"
     clicketi_click(context, clear_icons[0])
