102c6a872d914d4a947ddb1a132c794cc2da8acd
Dimitris Moraitis <dimo@mist.io>
2019-02-07 13:37:10 +0200

    Sleep before searching or clearing search

--- misttests/integration/gui/steps/search.py
+++ misttests/integration/gui/steps/search.py
@@ -8,6 +8,7 @@ from .utils import expand_shadow_root
 
 @step(u'I search for "{search_text}"')
 def search_for_something(context, search_text):
+    sleep(.5)
     mist_app = context.browser.find_element_by_tag_name('mist-app')
     mist_app_shadow = expand_shadow_root(context, mist_app)
     mist_header = mist_app_shadow.find_element_by_css_selector('mist-header')
@@ -27,6 +28,7 @@ def search_for_something(context, search_text):
 
 @step(u'I clear the search bar')
 def clear_search(context):
+    sleep(.5)
     mist_app = context.browser.find_element_by_tag_name('mist-app')
     mist_app_shadow = expand_shadow_root(context, mist_app)
     mist_header = mist_app_shadow.find_element_by_css_selector('mist-header')
@@ -36,6 +38,7 @@ def clear_search(context):
     mist_filter = top_search_shadow.find_element_by_css_selector('mist-filter')
     mist_filter_shadow = expand_shadow_root(context, mist_filter)
     mist_filter_shadow.find_element_by_css_selector('iron-icon').click()
+    sleep(.5)
     clear_icons = mist_filter_shadow.find_elements_by_css_selector(
         'paper-icon-button[icon="close"]')
     clear_icons = filter(lambda el: el.is_displayed(), clear_icons)
