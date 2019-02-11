0ad3f4e669725c9cf580bc39f00a6096c3aa6df7
Dimitris Moraitis <dimo@mist.io>
2019-02-11 15:58:05 +0200

    Fix typo

--- misttests/integration/gui/steps/navigation.py
+++ misttests/integration/gui/steps/navigation.py
@@ -230,7 +230,7 @@ def scroll_to_add_new_rule_btn(context):
 
 @step(u'I scroll to the rules section in the "{page}" page')
 def scroll_to_rules_section(context, page):
-    page_element = get_page(context, page)
+    page_element = get_page_element(context, page)
     page_shadow = expand_shadow_root(context, page_element)
     mist_rules = page_shadow.find_element_by_css_selector('mist-rules')
     context.browser.execute_script("arguments[0].scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'})", mist_rules)
