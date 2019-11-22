ba1d51bf5e1bb97a15d8392c4e2a0eaefa10e35d
mpempekos <spirostza77@hotmail.com>
2019-11-22 14:36:33 +0200

    Verify that request and observation logs are visible in dashboard. Rename Softlayer to IBM Cloud

--- misttests/integration/gui/steps/logs.py
+++ misttests/integration/gui/steps/logs.py
@@ -30,3 +30,27 @@ def check_log_main_page(context, position, time_entry):
 
     msg = "Log entry in position %s is %s" % (position, log_el_text)
     assert time_entry in log_el_text, msg
+
+
+@step(u'I should see a(n) "{log_type}" log entry of action "{action}" added "{time_entry}" in the dashboard page within {timeout} seconds')
+def check_log_entry_dashboard_page(context, log_type, action, time_entry, timeout):
+    mist_app = context.browser.find_element_by_tag_name('mist-app')
+    mist_app_shadow = expand_shadow_root(context, mist_app)
+    mist_header = mist_app_shadow.find_element_by_tag_name('app-header-layout')
+    iron_pages = mist_header.find_element_by_id('iron-pages')
+    pg_dashboard = iron_pages.find_element_by_tag_name('page-dashboard')
+    pg_dash_shadow = expand_shadow_root(context, pg_dashboard)
+    logs = pg_dash_shadow.find_element_by_id('logs')
+    logs_shadow = expand_shadow_root(context, logs)
+    grid = logs_shadow.find_element_by_id('grid')
+    grid_shadow = expand_shadow_root(context, grid)
+    table = grid_shadow.find_element_by_id('table')
+    items = table.find_element_by_id('items')
+    _timeout = time() + int(timeout)
+    while time() < _timeout:
+        log_elements = items.find_elements_by_tag_name('tr')
+        for log in log_elements:
+            if log_type in log.text and action in log.text and time_entry in log.text:
+                return
+
+    assert False, "Not found!"
