b33693d1dfe2df9e80b0da96ff4c393f3086555c
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-03-05 11:20:17 +0200

    Add script for vault login
    The purpose of this commit is two-fold. Enable logging into vault
    without having to call {run,trigger}_tests.sh. Also, have vault_login()
    defined in one place.

--- container/trigger_tests.sh
+++ container/trigger_tests.sh
@@ -5,36 +5,6 @@ export MIST_URL=${MIST_URL:-http://172.17.0.1}
 export VNC=${VNC:-}
 args=$@
 
-vault_login() {
-    if [ -z "$VAULT_CLIENT_TOKEN" ]
-    then
-        export vault_server=${VAULT_ADDR:-https://vault.ops.mist.io:8200}
-        echo Vault username:
-        read username
-        echo Vault password:
-        read -s password
-        export PYTHONIOENCODING=utf8
-
-        VAULT_CLIENT_TOKEN=$(
-          curl -sSk $vault_server/v1/auth/userpass/login/$username -d '{ "password": "'${password}'" }' |
-          python -c "
-import sys, json;
-res = json.load(sys.stdin)
-if res.get('auth'):
-    print(res['auth']['client_token'])"
-        )
-
-        if [[ -z "${VAULT_CLIENT_TOKEN// }" ]]
-        then
-            echo 'Wrong credentials given...'
-            vault_login
-        else
-            export VAULT_CLIENT_TOKEN
-            echo 'Successfully logged in.'
-        fi
-    fi
-}
-
 if [ -z "$VNC" ]
 then
     echo Headless mode
@@ -53,7 +23,8 @@ sleep 2
 
 cd /mist.tests
 clear
-vault_login
+
+source ./vault_login.sh
 
 ./run_tests.sh $args
 echo "./run_tests.sh $args" > ~/.bash_history
