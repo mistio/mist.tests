07ef71aa9d556aadfd4be4b174e6fdf2937efd48
Dimitris Moraitis <dimo@mist.io>
2021-08-14 17:27:18 +0300

    Fix org name init

--- misttests/integration/gui/core/environment.py
+++ misttests/integration/gui/core/environment.py
@@ -58,7 +58,7 @@ def before_all(context):
     context.mist_config['LDAP_MEMBER_USERNAME'] = config.LDAP_MEMBER_USERNAME
     context.mist_config['LDAP_MEMBER_PASSWORD'] = config.LDAP_MEMBER_PASSWORD
     context.mist_config['LOCAL'] = config.LOCAL
-    context.mist_config['ORG_NAME'] = config.ORG_NAME + str(random.randint(1, 10000000))
+    context.mist_config['ORG_NAME'] = (config.ORG_NAME or config.NAME) + str(random.randint(1, 10000000))
     context.mist_config['NON_STOP'] = '--stop' not in sys.argv
     context.mist_config['ERROR_NUM'] = 0
     context.mist_config['MIST_URL'] = config.MIST_URL
