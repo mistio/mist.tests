6e025178d3ea0d46296a7441309f758948b8ed48
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-10-06 16:57:58 +0300

    Add images v2 test setup module

---
+++ misttests/integration/api/main/v2/setup/images.py
@@ -0,0 +1,30 @@
+from time import sleep
+from misttests import config
+from misttests.integration.api.helpers import assert_response_ok
+from misttests.integration.api.mistrequests import MistRequests
+
+
+def setup(api_token):
+    add_cloud_request = {
+        "name": "example-cloud",
+        "provider": "google",
+        "credentials": {
+            "projectId": "projectId",
+            "privateKey": "privateKey",
+            "email": "email"
+        },
+    }
+    config.inject_vault_credentials(add_cloud_request)
+    uri = config.MIST_URL + '/api/v2/clouds'
+    request = MistRequests(
+        api_token=api_token, uri=uri, json=add_cloud_request)
+    response = request.post()
+    assert_response_ok(response)
+    sleep(120)
+
+
+def teardown(api_token):
+    uri = config.MIST_URL + '/api/v2/clouds/example-cloud'
+    request = MistRequests(api_token=api_token, uri=uri)
+    response = request.delete()
+    assert_response_ok(response)
