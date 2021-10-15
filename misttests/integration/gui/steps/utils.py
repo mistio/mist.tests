21161f90338d752a7823605cae7dbb583af70559
Eis <monoatomic.alex@gmail.com>
2021-10-15 22:56:52 +0300

    Fix typo

--- misttests/integration/gui/steps/utils.py
+++ misttests/integration/gui/steps/utils.py
@@ -253,7 +253,7 @@ def get_grid_items(context, grid):
     try:
         extra_items = context.browser.execute_script(
             'return arguments[0]._cache.itemCaches[0].items', grid)
-        if(type(ret) is dict and ret.get(length) != 0)
+        if type(ret) is dict and ret.get(length) != 0:
             return extra_times
         ret = ret + extra_items
     except Exception:
