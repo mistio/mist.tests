7cd35cc3db4d374d274d3df0344a3bd5dbbaddd7
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-10-26 17:41:44 +0300

    Create unique cloud, cluster names in setup()

--- misttests/integration/api/main/v2/setup/clusters.py
+++ misttests/integration/api/main/v2/setup/clusters.py
@@ -1,11 +1,15 @@
 from misttests import config
 from misttests.integration.api.helpers import assert_response_ok
+from misttests.integration.api.helpers import uniquify_string
 from misttests.integration.api.mistrequests import MistRequests
+
+CLOUDS_ENDPOINT = 'api/v2/clouds'
 
 
 def setup(api_token):
+    cloud_name = uniquify_string('test-cloud')
     add_cloud_request = {
-        "name": "example-cloud",
+        "name": cloud_name,
         "provider": "google",
         "credentials": {
             "projectId": "projectId",
@@ -17,16 +21,21 @@ def setup(api_token):
         },
     }
     config.inject_vault_credentials(add_cloud_request)
-    uri = config.MIST_URL + '/api/v2/clouds'
+    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}'
     request = MistRequests(
         api_token=api_token, uri=uri, json=add_cloud_request)
     response = request.post()
     assert_response_ok(response)
+    cluster_name = uniquify_string('test-cluster')
+    return {
+        'cluster': cluster_name,
+        'cloud': cloud_name
+    }
 
 
-def teardown(api_token):
-    uri = config.MIST_URL + '/api/v2/clouds/{cloud}'.format(
-        cloud="example-cloud")
+def teardown(api_token, setup_data):
+    cloud_name = setup_data['cloud']
+    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}/{cloud_name}'
     request = MistRequests(
         api_token=api_token, uri=uri)
     response = request.delete()
