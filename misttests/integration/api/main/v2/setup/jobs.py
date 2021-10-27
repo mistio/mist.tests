16c8c540c8b7eb0a02fe39c7a684c43a7d454591
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-10-27 22:30:05 +0300

    Update jobs v2 test setup module

--- misttests/integration/api/main/v2/setup/jobs.py
+++ misttests/integration/api/main/v2/setup/jobs.py
@@ -4,9 +4,16 @@ from misttests.integration.api.helpers import assert_response_ok
 from misttests.integration.api.helpers import uniquify_string
 from misttests.integration.api.mistrequests import MistRequests
 
+V1_ENDPOINT = 'api/v1'
+V2_ENDPOINT = 'api/v2'
+CLOUDS_V1_ENDPOINT = f'{V1_ENDPOINT}/clouds'
+CLOUDS_V2_ENDPOINT = f'{V2_ENDPOINT}/clouds'
+MACHINES_ENDPOINT = f'{V2_ENDPOINT}/machines'
+IMAGES_ENDPOINT = f'{V2_ENDPOINT}/images'
+
 
 def setup(api_token):
-    # Add a cloud
+    # Add cloud
     cloud_name = uniquify_string('test-cloud')
     add_cloud_request = {
         "name": cloud_name,
@@ -20,13 +27,13 @@ def setup(api_token):
         }
     }
     config.inject_vault_credentials(add_cloud_request)
-    uri = config.MIST_URL + '/api/v2/clouds'
+    uri = f'{config.MIST_URL}/{CLOUDS_V2_ENDPOINT}'
     request = MistRequests(
         api_token=api_token, uri=uri, json=add_cloud_request)
     response = request.post()
     assert_response_ok(response)
     # Get cloud id
-    cloud_uri = f'{config.MIST_URL}/api/v2/clouds/{cloud_name}'
+    cloud_uri = f'{config.MIST_URL}/{CLOUDS_V2_ENDPOINT}/{cloud_name}'
     request = MistRequests(
         api_token=api_token, uri=cloud_uri)
     response = request.get()
@@ -34,13 +41,13 @@ def setup(api_token):
     cloud_id = response.json().get('data', {}).get('id', '')
     # Get image id
     image_name = 'Debian Bullseye with SSH server'
-    image_uri = f'{config.MIST_URL}/api/v2/images/{image_name}'
+    image_uri = f'{config.MIST_URL}/{IMAGES_ENDPOINT}/{image_name}'
     request = MistRequests(
         api_token=api_token, uri=image_uri)
     response = request.get()
     assert_response_ok(response)
     image_id = response.json().get('data', {}).get('id', '')
-    # Create a machine
+    # Create machine
     machine_name = uniquify_string('test-machine')
     add_machine_request = {
         "name": machine_name,
@@ -49,13 +56,14 @@ def setup(api_token):
         "size": '',
         "async": True
     }
-    machines_uri = f'{config.MIST_URL}/api/v1/clouds/{cloud_id}/machines'
+    machines_uri = \
+        f'{config.MIST_URL}/{CLOUDS_V1_ENDPOINT}/{cloud_id}/machines'
     request = MistRequests(
         api_token=api_token, uri=machines_uri, json=add_machine_request)
     response = request.post()
     assert_response_ok(response)
     job_id = response.json().get('jobId') or response.json().get('job_id')
-    sleep(30)
+    sleep(60)
     return {
         'cloud': cloud_name,
         'machine': machine_name,
@@ -64,16 +72,16 @@ def setup(api_token):
 
 
 def teardown(api_token, setup_data):
-    # Destroy the machine
+    # Destroy machine
     machine_name = setup_data['machine']
-    uri = (f'{config.MIST_URL}/api/v2/machines'
+    uri = (f'{config.MIST_URL}/{MACHINES_ENDPOINT}'
            f'/{machine_name}/actions/destroy')
     request = MistRequests(api_token=api_token, uri=uri)
     response = request.post()
     assert_response_ok(response)
-    # Remove the cloud
+    # Remove cloud
     cloud_name = setup_data['cloud']
-    uri = f'{config.MIST_URL}/api/v2/clouds/{cloud_name}'
+    uri = f'{config.MIST_URL}/{CLOUDS_V2_ENDPOINT}/{cloud_name}'
     request = MistRequests(
         api_token=api_token, uri=uri)
     response = request.delete()
