44b657f1409e82cdd28c8617748d5bfdb8550ad0
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-11-07 18:26:54 +0200

    Add zones v2 test setup module

---
+++ misttests/integration/api/main/v2/setup/zones.py
@@ -0,0 +1,43 @@
+from misttests import config
+from misttests.integration.api.helpers import assert_response_ok
+from misttests.integration.api.helpers import uniquify_string
+from misttests.integration.api.mistrequests import MistRequests
+
+CLOUDS_ENDPOINT = 'api/v2/clouds'
+
+
+def setup(api_token):
+    cloud_name = uniquify_string('test-cloud')
+    zone_name = uniquify_string('test-zone') + '.com'
+    add_cloud_request = {
+        'name': cloud_name,
+        'provider': 'google',
+        'credentials': {
+            'projectId': 'projectId',
+            'privateKey': 'privateKey',
+            'email': 'email'
+        },
+    }
+    config.inject_vault_credentials(add_cloud_request)
+    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}'
+    request = MistRequests(
+        api_token=api_token, uri=uri, json=add_cloud_request)
+    response = request.post()
+    assert_response_ok(response)
+    request_body = {
+        'name': zone_name,
+        'cloud': cloud_name,
+        'type': 'master',
+        'ttl': '0'
+    }
+    return dict(overwrite_request=request_body,
+                cloud=cloud_name,
+                zone=zone_name)
+
+
+def teardown(api_token, setup_data):
+    cloud_name = setup_data['cloud']
+    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}/{cloud_name}'
+    request = MistRequests(
+        api_token=api_token, uri=uri)
+    request.delete()
