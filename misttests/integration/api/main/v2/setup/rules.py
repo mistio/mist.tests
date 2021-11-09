2e3e1e95407cd7c864c1743173e1f4832633b04c
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-11-10 01:47:13 +0200

    Fix rules v2 tests setup module

--- misttests/integration/api/main/v2/setup/rules.py
+++ misttests/integration/api/main/v2/setup/rules.py
@@ -1,34 +1,36 @@
 def setup(api_token):
-    request_body = {
-        'add_rule': {
-            'queries': [
-                {
-                    'target': 'type:session',
-                    'operator': 'gt',
-                    'threshold': 1,
-                    'aggregation': 'count'
-                }
-            ],
-            'actions': [
-                {
-                    'type': 'notification',
-                    'level': 'warning'
-                }
-            ],
-            'data_type': 'logs',
-            'window': {
-                'period': 'minutes',
-                'start': 1
-            },
-            'trigger_after': {
-                'offset': 0,
-                'period': 'seconds'
-            },
-            'frequency': {
-                'period': 'minutes',
-                'every': 1
+    query_request_body = {
+        'queries': [
+            {
+                'target': 'type:session',
+                'operator': 'gt',
+                'threshold': 1,
+                'aggregation': 'count'
             }
+        ],
+        'actions': [
+            {
+                'type': 'notification',
+                'level': 'warning'
+            }
+        ],
+        'data_type': 'logs',
+        'window': {
+            'period': 'minutes',
+            'start': 1
+        },
+        'trigger_after': {
+            'offset': 0,
+            'period': 'seconds'
+        },
+        'frequency': {
+            'period': 'minutes',
+            'every': 1
         }
+    }
+    request_body = {
+        'add_rule': query_request_body,
+        'edit_rule': query_request_body,
     }
     return dict(request_body=request_body,
                 rule='rule')
