5c5274a8fb88fc7756678a7abdeb4d5ab433ece3
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-11-26 21:57:19 +0200

    Update snapshots setup

--- misttests/integration/api/main/v2/setup/snapshots.py
+++ misttests/integration/api/main/v2/setup/snapshots.py
@@ -1,19 +1,15 @@
-from time import sleep
-from misttests import config
+from misttests.config import inject_vault_credentials
+from misttests.config import MIST_URL
+from misttests.integration.api.helpers import poll
 from misttests.integration.api.helpers import assert_response_ok
 from misttests.integration.api.helpers import uniquify_string
 from misttests.integration.api.mistrequests import MistRequests
 
-V1_ENDPOINT = 'api/v1'
 V2_ENDPOINT = 'api/v2'
-CLOUDS_V1_ENDPOINT = f'{V1_ENDPOINT}/clouds'
 CLOUDS_V2_ENDPOINT = f'{V2_ENDPOINT}/clouds'
 MACHINES_ENDPOINT = f'{V2_ENDPOINT}/machines'
 IMAGES_ENDPOINT = f'{V2_ENDPOINT}/images'
-
-
-class ImageNotFound(Exception):
-    pass
+VSPHERE_IMAGE = 'template'
 
 
 def setup(api_token):
@@ -29,72 +25,57 @@ def setup(api_token):
             'username': None
         }
     }
-    config.inject_vault_credentials(add_cloud_request)
-    uri = f'{config.MIST_URL}/{CLOUDS_V2_ENDPOINT}'
+    inject_vault_credentials(add_cloud_request)
+    uri = f'{MIST_URL}/{CLOUDS_V2_ENDPOINT}'
     request = MistRequests(
         api_token=api_token, uri=uri, json=add_cloud_request)
     response = request.post()
     assert_response_ok(response)
-    # Get cloud id
-    cloud_uri = f'{config.MIST_URL}/{CLOUDS_V2_ENDPOINT}/{cloud_name}'
-    request = MistRequests(
-        api_token=api_token, uri=cloud_uri)
-    response = request.get()
-    assert_response_ok(response)
-    cloud_id = response.json()['data']['id']
-    # Get image id
-    image_name = 'template'
-    image_uri = f'{config.MIST_URL}/{IMAGES_ENDPOINT}/{image_name}'
-    request = MistRequests(
-        api_token=api_token, uri=image_uri)
-
-    for _ in range(10):
-        response = request.get()
-        try:
-            assert_response_ok(response)
-            image_id = response.json()['data']['id']
-        except (KeyError, AssertionError):
-            sleep(10)
-        else:
-            break
-    else:
-        raise ImageNotFound()
-
+    # Wait for image to become available
+    assert poll(api_token=api_token,
+                uri=f'{MIST_URL}/{IMAGES_ENDPOINT}',
+                query_params=[('cloud', cloud_name)],
+                data={'name': VSPHERE_IMAGE})
     # Create machine
     machine_name = uniquify_string('test-machine')
     add_machine_request = {
         'name': machine_name,
-        'image': image_id,
-        'size': {'ram': 256, 'cpu': 1},
-        'async': True
+        'cloud': 'vsphere',
+        'image': VSPHERE_IMAGE,
+        'size': {'ram': 256, 'cpus': 1},
+        'dry': False
     }
-    machines_uri = \
-        f'{config.MIST_URL}/{CLOUDS_V1_ENDPOINT}/{cloud_id}/machines'
+    machines_uri = f'{MIST_URL}/{MACHINES_ENDPOINT}'
     request = MistRequests(
         api_token=api_token, uri=machines_uri, json=add_machine_request)
     response = request.post()
     assert_response_ok(response)
+    # Wait for machine to become available
+    assert poll(api_token=api_token,
+                uri=machines_uri,
+                query_params=[('cloud', cloud_name)],
+                data={'name': machine_name},
+                timeout=800)
     snapshot_name = uniquify_string('test-snapshot')
-    query_string = {'create_snapshot': [('name', snapshot_name)]}
-    sleep(200)
-    return {
+    setup_data = {
+        'create_snapshot': {'query_string': [('name', snapshot_name)]},
+        'snapshot': snapshot_name,
         'cloud': cloud_name,
         'machine': machine_name,
-        'snapshot': snapshot_name,
-        'query_string': query_string
     }
+    return setup_data
 
 
 def teardown(api_token, setup_data):
     # Destroy machine
     machine_name = setup_data['machine']
-    uri = (f'{config.MIST_URL}/{MACHINES_ENDPOINT}'
+    uri = (f'{MIST_URL}/{MACHINES_ENDPOINT}'
            f'/{machine_name}/actions/destroy')
     request = MistRequests(api_token=api_token, uri=uri)
     request.post()
     # Remove cloud
     cloud_name = setup_data['cloud']
-    uri = f'{config.MIST_URL}/{CLOUDS_V2_ENDPOINT}/{cloud_name}'
+    uri = f'{MIST_URL}/{CLOUDS_V2_ENDPOINT}/{cloud_name}'
     request = MistRequests(
         api_token=api_token, uri=uri)
     request.delete()
