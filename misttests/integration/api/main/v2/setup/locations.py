3c391eb5cd7cf0a25379b25ad61b7ddc8694240f
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-11-26 20:15:32 +0200

    Update locations setup

--- misttests/integration/api/main/v2/setup/locations.py
+++ misttests/integration/api/main/v2/setup/locations.py
@@ -1,5 +1,5 @@
-from time import sleep
-from misttests import config
+from misttests.config import inject_vault_credentials
+from misttests.config import MIST_URL
 from misttests.integration.api.helpers import assert_response_ok
 from misttests.integration.api.helpers import uniquify_string
 from misttests.integration.api.mistrequests import MistRequests
@@ -10,26 +10,29 @@ CLOUDS_ENDPOINT = 'api/v2/clouds'
 def setup(api_token):
     cloud_name = uniquify_string('test-cloud')
     add_cloud_request = {
-        "name": cloud_name,
-        "provider": "google",
-        "credentials": {
-            "projectId": "projectId",
-            "privateKey": "privateKey",
-            "email": "email"
+        'name': cloud_name,
+        'provider': 'google',
+        'credentials': {
+            'projectId': None,
+            'privateKey': None,
+            'email': None
         },
     }
-    config.inject_vault_credentials(add_cloud_request)
-    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}'
+    inject_vault_credentials(add_cloud_request)
+    uri = f'{MIST_URL}/{CLOUDS_ENDPOINT}'
     request = MistRequests(
         api_token=api_token, uri=uri, json=add_cloud_request)
     response = request.post()
     assert_response_ok(response)
-    sleep(120)
-    return {'cloud': cloud_name}
+    setup_data = {
+        'get_location': {'location': 'us-central1-c'},
+        'cloud': cloud_name
+    }
+    return setup_data
 
 
 def teardown(api_token, setup_data):
     cloud_name = setup_data['cloud']
-    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}/{cloud_name}'
+    uri = f'{MIST_URL}/{CLOUDS_ENDPOINT}/{cloud_name}'
     request = MistRequests(api_token=api_token, uri=uri)
     request.delete()
