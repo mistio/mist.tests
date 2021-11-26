21fc74a9f70a414413a64393be92b1e22febb3b5
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-11-26 18:53:51 +0200

    Update clusters setup

--- misttests/integration/api/main/v2/setup/clusters.py
+++ misttests/integration/api/main/v2/setup/clusters.py
@@ -1,43 +1,61 @@
-from misttests import config
+from functools import partial
+
+from misttests.config import inject_vault_credentials
+from misttests.config import MIST_URL
 from misttests.integration.api.helpers import assert_response_ok
+from misttests.integration.api.helpers import poll
 from misttests.integration.api.helpers import uniquify_string
 from misttests.integration.api.mistrequests import MistRequests
 
-CLOUDS_ENDPOINT = 'api/v2/clouds'
+V2_ENDPOINT = 'api/v2'
+CLOUDS_ENDPOINT = f'{V2_ENDPOINT}/clouds'
+CLUSTERS_ENDPOINT = f'{V2_ENDPOINT}/clusters'
 
 
 def setup(api_token):
     cloud_name = uniquify_string('test-cloud')
     add_cloud_request = {
-        "name": cloud_name,
-        "provider": "google",
-        "credentials": {
-            "projectId": "projectId",
-            "privateKey": "privateKey",
-            "email": "email"
+        'name': cloud_name,
+        'provider': 'google',
+        'credentials': {
+            'projectId': 'projectId',
+            'privateKey': 'privateKey',
+            'email': 'email'
         },
-        "features": {
-            "container": True,
+        'features': {
+            'container': True,
         },
     }
-    config.inject_vault_credentials(add_cloud_request)
-    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}'
+    inject_vault_credentials(add_cloud_request)
+    clouds_uri = f'{MIST_URL}/{CLOUDS_ENDPOINT}'
     request = MistRequests(
-        api_token=api_token, uri=uri, json=add_cloud_request)
+        api_token=api_token, uri=clouds_uri, json=add_cloud_request)
     response = request.post()
     assert_response_ok(response)
     cluster_name = uniquify_string('test-cluster')
-    create_cluster = {'sleep': 300}
-    destroy_cluster = {'sleep': 150}
-    return dict(create_cluster=create_cluster,
-                destroy_cluster=destroy_cluster,
-                cluster=cluster_name,
-                cloud=cloud_name)
+    cluster_uri = f'{MIST_URL}/{CLUSTERS_ENDPOINT}/{cluster_name}'
+    test_args = {
+        'create_cluster': {
+            'request_body': {
+                'cloud': cloud_name,
+                'location': 'us-central1-c',
+                'name': cluster_name,
+                'provider': 'google'
+            },
+            'callback': partial(poll,
+                                api_token=api_token,
+                                uri=cluster_uri,
+                                data={'name': cluster_name},
+                                post_delay=120),
+        },
+        'destroy_cluster': {'cluster': cluster_name}
+    }
+    setup_data = dict(**test_args, cloud=cloud_name)
+    return setup_data
 
 
 def teardown(api_token, setup_data):
     cloud_name = setup_data['cloud']
-    uri = f'{config.MIST_URL}/{CLOUDS_ENDPOINT}/{cloud_name}'
-    request = MistRequests(
-        api_token=api_token, uri=uri)
+    cloud_uri = f'{MIST_URL}/{CLOUDS_ENDPOINT}/{cloud_name}'
+    request = MistRequests(api_token=api_token, uri=cloud_uri)
     request.delete()
