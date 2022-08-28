d1bc8fbaacf221d7252f0629a97db6436c0e331b
Dimitris Moraitis <dimo@mist.io>
2022-08-28 14:16:00 +0300

    Add missing setup files

---
+++ misttests/integration/api/main/v2/setup/datapoints.py
@@ -0,0 +1,11 @@
+# from misttests.config import MIST_URL
+# from misttests.integration.api.helpers import assert_response_ok
+# from misttests.integration.api.mistrequests import MistRequests
+
+
+def setup(api_token):
+    pass
+
+
+def teardown(api_token, setup_data):
+    pass
