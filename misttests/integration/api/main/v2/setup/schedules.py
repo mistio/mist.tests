e2adcf73582785931e6074dd6f6f0ac3a4768509
gkaps <gkaps@mist.io>
2022-09-02 20:42:56 +0300

    Add when on add_schedule payload

--- misttests/integration/api/main/v2/setup/schedules.py
+++ misttests/integration/api/main/v2/setup/schedules.py
@@ -88,7 +88,11 @@ def setup(api_token):
                 'run_immediately' : True,
                 'selectors' : [ {'ids':[machine_id], 'type': 'machines'}],
                 'actions' : [{'action_type':'reboot'}],
-                'enabled' : True
+                'enabled' : True,
+                'when': {
+                    'schedule_type' : 'one_off',
+                    'datetime' : (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d %H:%m:%S")
+                }
             },
         },
         'edit_schedule': {
